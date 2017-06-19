b3a4d84dba08a5bff7ca8c1354a9feb610cb8cb7
Olalla <olalla@superoliva.es>
2017-06-19 18:20:43 +0200

    i

---
+++ index.php
@@ -0,0 +1,11 @@
+<html><head>
+<meta http-equiv="content-type" content="text/html; charset=windows-1252">
+<title>Emeceworks</title>
+</head>
+ 
+<body>
+<h1 style="font-size:100px;margin:100px auto;color:red;">Hola Mundo, estoy servidor emeceworks... algo te salio mal...</h1>
+
+ 
+
+</body></html>
