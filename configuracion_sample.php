bf784b60818a17033bed4385325ff2d9e848b78d
Ricardo Oficina <ricardolau6@gmail.com>
2017-06-26 20:55:06 +0200

    Los cambisos de configuracion

--- configuracion_sample.php
+++ configuracion_sample.php
@@ -1,8 +1,5 @@
 <?php
-$HostNombre = '/beta/catalogoproductos';	// ruta instalacion de proyecto
+$HostNombre = '/superoliva/tpvricardo';	// ruta instalacion de proyecto
 $RutaServidor= '/home/solucion40/www';		// ruta servidor
-$ConfDir_subida = '/tmp/'; 					// Lugar donde el servidor indica que guarda los tmp
-$nombreBDJoomla = 'base_datos_Joomla';		// Nombre de base datos que tiene Joomla y virtuemart
-$prefijoJoomla = 'prefijo_tablas';			// Prefijo tiene joomla en la tablas.
-$hostTienda = 'urldeJoomla'; 				// Variable que utilizamos para saber donde esta alojada la web... servidor
+$CopiaDBF = '/superoliva/datos/DBF71';	// Lugar donde copiamos DBF. Recuerda que tiene que tener permisos Apache, sino va...
 ?>
