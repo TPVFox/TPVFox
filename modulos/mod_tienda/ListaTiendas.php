5850d5a720b7eb3f36ec7b0036219911f80b9bbb
Olalla <olalla@superoliva.es>
2017-09-11 21:38:24 +0200

    modulo tienda, lista tiendas y crea o modifica tienda

---
+++ modulos/mod_tienda/ListaTiendas.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php
+	include './../../head.php';
+		if ($Usuario['estado'] === "Incorrecto"){
+		return;	
+	}
+
+	
+	include './funciones.php';
+	$tiendas = obtenerTiendas($BDTpv);
+	
+	?>
+	<script>
+	// Declaramos variables globales
+	var checkID = [];
+	var BRecambios ='';
+	</script> 
+    <!-- Cargamos fuciones de modulo. -->
+	<script src="<?php echo $HostNombre; ?>/modulos/mod_tienda/funciones.js"></script>
+    
+    <!-- Cargamos libreria control de teclado -->
+	<script src="<?php echo $HostNombre; ?>/lib/shortcut.js"></script>
+  
+	
+	<script>
+	// Funciones para atajo de teclado.
+	//~ shortcut.add("Shift+V",function() {
+		//~ // Atajo de teclado para ver
+		//~ metodoClick('VerUsuario');
+	//~ });    
+	    
+	</script> 
+    </head>
+
+<body>
+        <?php
+        include './../../header.php';
+        ?>
+       
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 text-center">
+					<h2> Tiendas: Editar y Añadir Tiendas </h2>
+					<?php 
+					//~ echo 'Numero filas'.$Familias->num_rows.'<br/>';
+					//~ echo '<pre class="text-left">';
+					//~ print_r($Familias);
+					//~ 
+					//~ echo '</pre>';
+					?>
+				</div>
+	        <!--=================  Sidebar -- Menu y filtro =============== 
+				Efecto de que permanezca fixo con Scroll , el problema es en
+				movil
+	        -->
+	       
+			<nav class="col-sm-2" id="myScrollspy">
+				<div data-spy="affix" data-offset-top="505">
+				<h4> Tiendas</h4>
+				<h5> Opciones para una selección</h5>
+				<ul class="nav nav-pills nav-stacked"> 
+					<li><a href="#section1" onclick="metodoClick('AgregarTienda');";>Añadir</a></li>
+					<li><a href="#section2" onclick="metodoClick('VerTienda');";>Modificar</a></li>
+									<?php //metodoClick js case pulsado 
+									//agregarUsuario nos lleva a formulario usuario
+									//verUsuario si esta checkado nos lleva vista usuario de ese id
+												//si NO nos indica que tenemos que elegir uno de la lista ?>
+				</ul>
+				</div>	
+			</nav>		
+			<div class="col-md-10">
+				<?php
+				//~ echo '<pre>';
+					//~ print_r($usuarios);
+				//~ echo '</pre>';
+				?>
+                 <!-- TABLA DE PRODUCTOS -->
+			<div>
+			<table class="table table-striped">
+				<thead>
+					<tr>
+						<th></th>
+						<th>ID</th>
+						<th>NOMBRE COMERCIAL</th>
+						<th>RAZON SOCIAL</th>
+						<th>NIF</th>
+						<th>DIRECCION</th>
+						<th>ANO</th>
+						<th>ESTADO</th>
+					</tr>
+				</thead>
+	
+				<?php
+				$checkUser = 0;
+				foreach ($tiendas['items'] as $tienda){ 
+					$checkUser = $checkUser + 1; 
+				?>
+
+				<tr>
+					<td class="rowTienda"><input type="checkbox" name="checkUsu<?php echo $checkUser;?>" value="<?php echo $tienda['idTienda'];?>">
+					</td>
+					<td><?php echo $tienda['idTienda']; ?></td>
+					<td><?php echo $tienda['NombreComercial']; ?></td>
+					<td><?php echo $tienda['razonsocial']; ?></td>
+					<td><?php echo $tienda['nif']; ?></td>
+					<td><?php echo $tienda['direccion']; ?></td>
+					<td><?php echo $tienda['ano']; ?></td>
+					<td><?php echo $tienda['estado']; ?></td>
+					
+				</tr>
+
+				<?php 
+				}
+				?>
+				
+			</table>
+			</div>
+		</div>
+	</div>
+    </div>
+		
+</body>
+</html>
---
+++ modulos/mod_tienda/funciones.js
@@ -0,0 +1,63 @@
+// Funciones modulo de recambios vista lista recambios.
+//funcion usada en js case metodoclick para el check de la lista
+function VerTiendasSeleccionado (){
+	$(document).ready(function()
+	{
+		// Array para meter lo id de los checks
+		
+		// Contamos check están activos.... 
+		checkID = [] ; // Reiniciamos varible global.
+		var i= 0;
+		// Con la funcion each hace bucle todos los que encuentra..
+		$(".rowTienda").each(function(){ 
+			i++;
+			//todos los que sean de la clase row1
+			if($('input[name=checkUsu'+i+']').is(':checked')){
+				// cant cuenta los que está seleccionado.
+				valor = '0';
+				valor = $('input[name=checkUsu'+i+']').val();
+				checkID.push( valor );
+			}
+			
+		});
+		console.log('ID de Tienda seleccionado:'+checkID);
+		return;
+	});
+
+
+}
+
+function metodoClick(pulsado){
+	console.log("Inicimos switch de control pulsar");
+	switch(pulsado) {
+		case 'VerTienda':
+			console.log('Entro en VerUsuario');
+			// Cargamos variable global ar checkID = [];
+			VerTiendasSeleccionado ();
+			if (checkID.length >1 || checkID.length=== 0) {
+				alert ('Que items tienes seleccionados? \n Solo puedes tener uno seleccionado');
+				return
+			}
+			// Ahora redireccionamos 
+			// recambi.php?id=id
+				
+			window.location.href = './tienda.php?id='+checkID[0];
+			
+			
+			break;
+		
+		case 'AgregarTienda':
+			console.log('entro en agregarUsuario');
+			window.location.href = './tienda.php';
+			
+			break;
+		
+		
+		
+	 }
+} 
+
+
+
+
+
