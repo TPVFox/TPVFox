ed866cb6bc8e7ce68f732c9f38047cb40a7b723c
Olalla <olalla@superoliva.es>
2017-09-11 21:15:00 +0200

    estructura cambiada estado varchar 8

--- BD/BDtpv/tiendas.sql
+++ BD/BDtpv/tiendas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:3306
--- Tiempo de generación: 11-09-2017 a las 20:13:57
+-- Tiempo de generación: 11-09-2017 a las 21:12:09
 -- Versión del servidor: 10.1.26-MariaDB-0+deb9u1
 -- Versión de PHP: 7.0.19-1
 
@@ -33,15 +33,8 @@ CREATE TABLE `tiendas` (
   `nif` varchar(10) NOT NULL,
   `direccion` varchar(100) NOT NULL,
   `ano` varchar(4) DEFAULT NULL,
-  `estado` varchar(6) DEFAULT NULL
+  `estado` varchar(8) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `tiendas`
---
-
-INSERT INTO `tiendas` (`idTienda`, `NombreComercial`, `razonsocial`, `nif`, `direccion`, `ano`, `estado`) VALUES
-(1, 'Supermercados Oliva', 'Alimentaria Longueicarp', 'B36937100', 'Emilia Pardo Bazan', '2004', NULL);
 
 --
 -- Índices para tablas volcadas
@@ -61,7 +54,7 @@ ALTER TABLE `tiendas`
 -- AUTO_INCREMENT de la tabla `tiendas`
 --
 ALTER TABLE `tiendas`
-  MODIFY `idTienda` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idTienda` int(2) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
