b218f008bf1fca4a2cccd637396a018cea198edd
Ricardo Oficina <ricardolau6@gmail.com>
2017-09-27 11:48:51 +0200

    Arreglo que se pierda el link item de menu de documentacion

--- header.php
+++ header.php
@@ -24,7 +24,7 @@
 				<ul class="nav navbar-nav navbar-left ">
 					<li><a href="<?php echo $HostNombre.'/index.php'?>">Home</a></li>
 					<li><a href="<?php echo $HostNombre.'/modulos/mod_recambios/ListaArticulos.php';?>">Articulos</a></li>
-					<li><a href="./estatico">Documentacion</a></li>
+					<li><a href="<?php echo $HostNombre.'/estatico';?>">Documentacion</a></li>
 					<li><a href="<?php echo $HostNombre.'/modulos/mod_tpv/tpv.php';?>">Tickets</a></li>
 					<li><a href="<?php echo $HostNombre.'/modulos/mod_usuario/ListaUsuarios.php';?>">Usuarios</a></li>
 					<li><a href="<?php echo $HostNombre.'/modulos/mod_tienda/ListaTiendas.php';?>">Tiendas</a></li>
