822f9d6739f79c670fd4d97e2d2f4987d160665f
Ricardo Oficina <ricardolau6@gmail.com>
2017-10-03 00:13:53 +0200

    - Arreglo error de tener lineas y ivas de ticket con el mismo numero, ya que es posible con distintos usuarios y distintas tiendas. La solución fue utilizar idticketst.
     Cambios para hacer commit:
    	modificado: BD/BDtpv/ticketslinea.sql
    	modificado: BD/BDtpv/ticketstIva.sql
    	modificado: modulos/mod_tpv/funciones.php

--- BD/BDtpv/ticketstIva.sql
+++ BD/BDtpv/ticketstIva.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 26-09-2017 a las 17:21:25
+-- Tiempo de generación: 03-10-2017 a las 00:12:23
 -- Versión del servidor: 5.5.57-0ubuntu0.14.04.1
 -- Versión de PHP: 5.6.31-4+ubuntu14.04.1+deb.sury.org+4
 
@@ -28,12 +28,13 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `ticketstIva` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idticketst` int(11) NOT NULL,
   `Numticket` int(11) NOT NULL,
   `iva` int(11) NOT NULL,
   `importeIva` decimal(17,2) NOT NULL,
   `totalbase` decimal(17,2) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=219 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=286 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
