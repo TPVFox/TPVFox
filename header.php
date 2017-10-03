d477909b53e518e98233c15c964eed19c24ed364
Olalla <olalla@superoliva.es>
2017-10-03 21:55:38 +0200

    coloco dropbox para ver importar con mas opciones en menu lista

--- header.php
+++ header.php
@@ -49,9 +49,16 @@
 					<?php if ($Usuario['group_id'] === '1'){?>
 					<li><a href="<?php echo $HostNombre.'/modulos/mod_usuario/ListaUsuarios.php';?>">Usuarios</a></li>
 					<li><a href="<?php echo $HostNombre.'/modulos/mod_tienda/ListaTiendas.php';?>">Tiendas</a></li>
-					<li><a href="<?php echo $HostNombre.'/modulos/mod_importar_sppg/Importar_sppg.php';?>">Importar SPPG</a></li>
-					<li><a href="<?php echo $HostNombre.'/modulos/mod_importar_virtuemart/Importar_virtuemart.php';?>">Importar Virtuemart</a></li>
-
+					<?php //coloco dropdown importar, al pinchar tengo 2 opc en lista?>
+					<li class="dropdown">
+						<a class="dropdown-toggle" data-toggle="dropdown" href="#">Importar
+						<span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<li><a href="<?php echo $HostNombre.'/modulos/mod_importar_sppg/Importar_sppg.php';?>">Importar SPPG</a></li>
+							<li><a href="<?php echo $HostNombre.'/modulos/mod_importar_virtuemart/Importar_virtuemart.php';?>">Importar Virtuemart</a></li>
+						</ul>
+					</li>
+					
 					<?php } ;?>
 				</ul>
 				
