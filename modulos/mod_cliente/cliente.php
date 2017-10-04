3dbe7f9009d72071cf78d5a8ea5b04c031671158
Olalla <olalla@superoliva.es>
2017-10-04 23:25:25 +0200

    listo, creo y modifico clientes. muestro paginacion y filtro de busqueda

---
+++ modulos/mod_cliente/cliente.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php
+		// Reinicio variables
+        include './../../head.php';
+        include './funciones.php';
+        include ("./../mod_conexion/conexionBaseDatos.php");
+		if ($Usuario['estado'] === "Incorrecto"){
+			return;	
+		}
+		
+		?>
+		<!-- Cargamos libreria control de teclado -->
+		
+		
+	</head>
+	<body>
+		<?php
+        include './../../header.php';
+		// ===========  datos cliente segun id enviado por url============= //
+		$idTienda = $Tienda['idTienda'];
+		$tabla= 'clientes'; // Tablas que voy utilizar.
+		$estados = array(); // Creo los estados de usuarios ( para select)
+		$estados[0]['valor'] = 'inactivo'; // Por defecto
+		$estados[1]['valor'] = 'activo';
+		// Obtenemos id
+		//~ print_r($_GET);
+		
+		
+		if (isset($_GET['id'])) {
+			// Modificar Ficha Cliente
+			$id=$_GET['id']; // Obtenemos id para modificar.
+			$ClienteUnico = verSelec($BDTpv,$id,$tabla);
+			$titulo = "Modificar Cliente";
+			if (isset($ClienteUnico['error'])){
+				$error='NOCONTINUAR';
+				$tipomensaje= "danger";
+				$mensaje = "Id de usuario incorrecto ( ver get) <br/>".$ClienteUnico['consulta'];
+			} else {
+				
+				// Ahora ponemos el estado por defecto segun el dato obtenido en la BD .
+				if (count($_POST) ===0){
+				$i = 0;
+					foreach ($estados as $estado){
+						if ($ClienteUnico['estado'] === $estado['valor']){
+						$estados[$i]['porDefecto'] = "selected"; // Indicamos por defecto
+						}
+					$i++;
+					}
+				} 
+			}
+		} else {
+			// Creamos ficha Usuario.
+			$titulo = "Crear Cliente";
+			$ClienteUnico = array();
+			$ClienteUnico['Nombre'] = '';
+			$ClienteUnico['razonsocial'] = '';
+			$ClienteUnico['nif'] = '';
+			$ClienteUnico['direccion'] = '';
+			$ClienteUnico['telefono'] = '';
+			$ClienteUnico['movil'] = '';
+			$ClienteUnico['fax'] = '';
+			$ClienteUnico['email'] = '';			
+			$estados[0]['porDefecto'] = "selected"; // Indicamos por defecto
+			//$ClienteUnico['id']= '';
+		}
+		
+		if (!isset($error)){
+			if(count($_POST)>0){
+				// Ya enviamos el formulario y gestionamos lo enviado.
+				$datos = $_POST;
+				if($titulo === "Crear Cliente"){
+					// Quiere decir que ya cubrimos los datos del usuario nuevo.
+					$resp = insertarCliente($datos,$BDTpv,$tabla);
+					echo $resp['sql'];
+					if (isset($resp['error'])){
+						$tipomensaje= "danger";
+						$mensaje = "Nombre de cliente ya existe!";
+						
+					} else {
+						$tipomensaje= "info";
+						$mensaje = "Nuevo cliente creado.";
+					}
+				} else {
+					// Quiere decir que ya modificamos los datos del ficha del cliente
+					$ClienteUnico['razonsocial'] =$datos['razonsocial'];
+					//~ echo'<pre>';
+					//~ print_r($datos);
+					//~ echo '</pre>';
+					$resp = modificarCliente($datos,$BDTpv,$tabla);
+					
+					if (isset($resp['error'])){
+						// Error de usuario repetido...
+						$tipomensaje= "danger";
+						$mensaje = "Razon social de cliente ya existe!";
+					} else {
+						$tipomensaje= "info";
+						$mensaje = "Su registro de cliente fue editado.";
+					}
+				};
+			}
+		}
+		
+		?>
+     
+		<div class="container">
+				
+			<?php 
+			//~ echo '<pre>';
+			//~ print_r($_POST);
+			//~ echo '</pre>';
+			if (isset($mensaje) || isset($error)){   ?> 
+				<div class="alert alert-<?php echo $tipomensaje; ?>"><?php echo $mensaje ;?></div>
+				<?php 
+				if (isset($error)){
+				// No permito continuar, ya que hubo error grabe.
+				return;
+				}
+				?>
+			<?php
+			}
+			?>
+			<h1 class="text-center"> <?php echo $titulo;?></h1>
+			<a class="text-ritght" href="./ListaClientes.php">Volver Atrás</a>
+			<div class="col-md-12">
+				
+				<h3><?php echo $ClienteUnico['razonsocial'];?></h3>
+
+				<div class="col-md-3">
+					<?php 
+					// UrlImagen
+					$img = './../../css/img/imgUsuario.png';
+					?>
+					<img src="<?php echo $img;?>" style="width:100%;">
+				</div>
+
+				<form action="" method="post" name="formCliente">
+				<div class="col-md-9">
+					<div class="Datos">
+						<div class="col-md-6 form-group">
+							<label>Nombre Cliente:</label>
+							<input type="text" id="nombre" name="nombre" <?php echo $ClienteUnico['Nombre'];?> placeholder="nombre" value="<?php echo $ClienteUnico['Nombre'];?>"   >
+							
+						</div>
+						<div class="col-md-6 form-group">
+							<label>Razon Social:</label> <!--//al enviar con POST los inputs se cogen con name="xx" PRE-->
+							<input type="text" id="razonsocial" name="razonsocial" placeholder="razon social" value="<?php echo $ClienteUnico['razonsocial'];?>"   >
+							
+						</div>
+						<div class="col-md-6 form-group">
+							<label>NIF:</label>
+							<input type="text"	id="nif" name="nif" value="<?php echo $ClienteUnico['nif'];?>" required>
+						</div>
+						<div class="col-md-6 form-group">
+							<label>Id del cliente:</label>
+							<input type="text" id="idCliente" name="idCliente" value="<?php echo $ClienteUnico['idClientes'];?>"   readonly>
+							
+						</div>
+						<div class="col-md-6 form-group">
+							<label>Direccion:</label>
+							<input type="text" id="direccion" name="direccion" value="<?php echo $ClienteUnico['direccion'];?>"   >
+						</div>
+						<div class="col-md-6 form-group">
+							<label>Telefono:</label>
+							<input type="text" id="telefono" name="telefono" value="<?php echo $ClienteUnico['telefono'];?>"   >
+						</div>
+						<div class="col-md-6 form-group">
+							<label>Movil:</label>
+							<input type="text" id="movil" name="movil" value="<?php echo $ClienteUnico['movil'];?>"   >
+						</div>
+						<div class="col-md-6 form-group">
+							<label>Fax:</label>
+							<input type="text" id="fax" name="fax" value="<?php echo $ClienteUnico['fax'];?>"   >
+						</div>
+						<div class="col-md-6 form-group">
+							<label>Email:</label>
+							<input type="text" id="email" name="email" value="<?php echo $ClienteUnico['email'];?>"  >
+						</div>
+						
+						<div class="col-md-6 form-group">
+							<label for="sel1">Estado:</label>
+							<select class="form-control" name="estado" id="sel1">
+								<?php 
+								foreach ($estados as $estado){
+								?>
+									<option value="<?php echo $estado['valor'];?>" <?php echo (isset($estado['porDefecto']) ? $estado['porDefecto'] : '');?> >
+									<?php echo $estado['valor'];?>
+									</option>
+								<?php
+								}
+								?>
+								
+							</select>
+						</div>
+						
+						
+					</div>
+					
+				</div>
+				<div class="col-md-12">
+					<input type="submit" value="Guardar">
+				
+				<div class="col-md-9">
+				</form>
+			</div>
+			
+		</div>
+	</body>
+</html>
---
+++ modulos/mod_cliente/funciones.php
@@ -0,0 +1,138 @@
+<?php 
+
+
+function obtenerClientes($BDTpv,$LimitePagina ,$desde,$filtro) {
+	// Function para obtener clientes y listarlos
+	//tener en cuenta el  paginado con parametros: $LimitePagina ,$desde,$filtro
+
+//para evitar repetir codigo
+	$Controler = new ControladorComun; 
+	$campoBD = 'nombre';
+	$campo2BD = 'razonsocial';
+	$rangoFiltro = $Controler->paginacionFiltroBuscar($BDTpv,$filtro,$LimitePagina,$desde,$campoBD,$campo2BD);
+	$rango=$rangoFiltro['rango'];
+	$filtroFinal=$rangoFiltro['filtro'];
+//fin paginacion y filtro de busqueda 
+
+
+	$clientes = array();
+	$consulta = "Select * from clientes ".$filtroFinal.$rango; 
+	$Resql = $BDTpv->query($consulta);
+	$clientes['NItems'] = $Resql->num_rows;
+	$i = 0;
+//~ echo '<pre>';
+//~ echo $consulta;
+//~ echo '</pre>';	
+	while ($cliente = $Resql->fetch_assoc()) {			
+			$clientes['items'][$i]['id'] = $cliente['idClientes'];
+			$clientes['items'][$i]['nombre'] = $cliente['Nombre'];
+			$clientes['items'][$i]['razonsocial'] = $cliente['razonsocial'];
+			$clientes['items'][$i]['nif'] = $cliente['nif'];
+			$clientes['items'][$i]['direccion'] = $cliente['direccion'];
+			$clientes['items'][$i]['telefono'] = $cliente['telefono'];
+			$clientes['items'][$i]['movil'] = $cliente['movil'];
+			$clientes['items'][$i]['fax'] = $cliente['fax'];
+			$clientes['items'][$i]['email'] = $cliente['email'];
+			$clientes['items'][$i]['estado'] = $cliente['estado'];
+	$i = $i+1;
+	}
+
+	$clientes ['consulta'] = $consulta;
+	return $clientes;
+}
+
+function verSelec($BDTpv,$idSelec,$tabla){
+	//ver seleccionado en check listado	
+	// Obtener datos de un id de usuario.
+	$where = 'idClientes = '.$idSelec;
+	$consulta = 'SELECT * FROM '. $tabla.' WHERE '.$where;
+	
+	$unaOpc = $BDTpv->query($consulta);
+	if (mysqli_error($BDTpv)) {
+		$fila['error'] = 'Error en la consulta '.$BDTpv->errno;
+	} else {
+		if ($unaOpc->num_rows > 0){
+			$fila = $unaOpc->fetch_assoc();
+		} else {
+			$fila['error']= ' No se a encontrado cliente';
+		}
+	}
+	$fila['Nrow']= $unaOpc->num_rows;
+	$fila['sql'] = $consulta;
+	return $fila ;
+}
+
+function insertarCliente($datos,$BDTpv,$tabla){
+	$resultado = array();
+	$nombre = $datos['nombre'];  //conseguir todos los nombres de la tabla, recorrerlos y comprobar q no existe
+	$razonsocial =$datos['razonsocial'];
+	$nif = $datos['nif'];
+	$direccion = $datos['direccion']; 
+	
+	$telefono=$datos['telefono'];
+	$movil = $datos['movil'];
+	$fax = $datos['fax'];
+	$email =$datos['email'];
+	$estado =$datos['estado'];
+	
+	//comprobar que razonsocial NO EXISTE al crear un nuevo usuario
+	$buscarCliente = 'SELECT * FROM clientes WHERE razonsocial= "'.$razonsocial.'"';
+	$res = $BDTpv->query($buscarCliente);
+	$numClientes = mysqli_num_rows($res); //num usuarios que existen con ese nombre
+	if ($numClientes === 1){
+		// Si entro es porque existe ya el usuario o no mando nombre usuario
+		$resultado['error'] = 'error';
+		//razon social YA EXISTE	
+	} else {
+		$consulta = 'INSERT INTO '.$tabla.'( Nombre, razonsocial, nif, direccion, telefono, movil, fax, email, estado ) VALUES ("'
+			.$nombre.'" , "'.$razonsocial.'" , "'.$nif.'" , "'.$direccion.'" , "'.$telefono.'" , "'.$movil.'" , "'.$fax.'" , "'.$email.'" , "'.$estado.'")';
+		if ($BDTpv->query($consulta) === true){
+			$resultado['id'] = $BDTpv->insert_id;	//crea id en bbddd 
+		} else {
+			// Quiere decir que hubo error en insertar en clientes
+			$resultado['error'] = 'Error en Insert de cliente Numero error:'.$BDTpv->errno;
+			$resultado['sql'] = $consulta;
+
+		}
+		
+	}
+	//~ $resultado['consulta'] =$InsertSlq;
+	return $resultado;
+}
+
+
+//parametros: 
+//datos array de post ,importante NAME de los inputs
+//BDTpv conexion bbdd tpv
+//tabla en la que trabajar usuarios
+//idSelecc , usuario concreto a modificar , check seleccionado en listaUsuarios
+function modificarCliente($datos,$BDTpv,$tabla){
+	
+	//~ echo 'modificar usuario';
+	$resultado = array();	
+	$nombre = $datos['nombre'];  //conseguir todos los nombres de la tabla, recorrerlos y comprobar q no existe
+	$razonsocial =$datos['razonsocial'];
+	$nif = $datos['nif'];
+	$direccion = $datos['direccion']; 
+	
+	$idCliente =$datos['idCliente'];
+	$telefono=$datos['telefono'];
+	$movil = $datos['movil'];
+	$fax = $datos['fax'];
+	$email =$datos['email'];
+	$estado =$datos['estado'];
+	
+	$sql ='UPDATE '.$tabla.' SET Nombre ="'.$nombre.'" , razonsocial ="'.$razonsocial.'" , nif ="'
+			.$nif.'" , direccion = "'.$direccion.'" , telefono ="'.$telefono.'" , movil ="'.$fax.'" , email ="'.$email.'" , estado = "'
+		.$estado.'" WHERE idClientes='.$idCliente;
+	
+	
+	$consulta = $BDTpv->query($sql);
+	
+	//$resultado['consulta'] =$sql;
+	$resultado['sql'] =$sql;
+
+	return $resultado;
+}
+
+?>
