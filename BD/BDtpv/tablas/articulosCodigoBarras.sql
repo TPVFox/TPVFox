3df5f31085cd46e7c2bbbe74f334bfef4ae9a72e
Olalla <olalla@superoliva.es>
2017-10-17 17:39:16 +0200

    - Eliminamos indices de articulosCodigoBarras. 	modified:   BD/BDtpv/tablas/articulosCodigoBarras.sql

--- BD/BDtpv/tablas/articulosCodigoBarras.sql
+++ BD/BDtpv/tablas/articulosCodigoBarras.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10deb1
--- http://www.phpmyadmin.net
+-- version 4.6.6deb4
+-- https://www.phpmyadmin.net/
 --
--- Servidor: localhost
--- Tiempo de generación: 02-10-2017 a las 00:49:32
--- Versión del servidor: 5.5.57-0ubuntu0.14.04.1
--- Versión de PHP: 5.6.31-4+ubuntu14.04.1+deb.sury.org+4
+-- Servidor: localhost:3306
+-- Tiempo de generación: 17-10-2017 a las 17:35:20
+-- Versión del servidor: 10.1.26-MariaDB-0+deb9u1
+-- Versión de PHP: 7.0.19-1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de datos: `tpv`
@@ -26,11 +26,9 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `articulosCodigoBarras`
 --
 
-CREATE TABLE IF NOT EXISTS `articulosCodigoBarras` (
+CREATE TABLE `articulosCodigoBarras` (
   `idArticulo` int(11) NOT NULL,
-  `codBarras` varchar(18) NOT NULL,
-  PRIMARY KEY (`codBarras`),
-  UNIQUE KEY `codBarras` (`codBarras`)
+  `codBarras` varchar(18) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
