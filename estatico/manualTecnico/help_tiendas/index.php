c9fc4a5433a808e73d007abee29a3c7c764e0af8
Ricardo Oficina <ricardolau6@gmail.com>
2017-10-29 20:02:02 +0100

    Añado ayuda de tienda , que no termine diagrama. :-)

---
+++ estatico/manualTecnico/help_tiendas/index.php
@@ -0,0 +1,21 @@
+<!-- Ayuda técnica de modulo tienda --> 
+<div>
+	<nav class="col-sm-2">
+		<a class="text-ritght" href="./">Volver Atrás</a>
+	</nav>
+	<div class="col-md-12">
+		<h2>Modulo de tienda</h2>
+		<p>El codigo de este modulo se encuentra en <b>directorio modulos/mod_tienda</b>, donde podremos crear nuevas tiendas, de momento el concepto de tienda.</p>
+		<p>
+		Concepto y obeservaciones que debemo saber de tienda : </p>
+		<ul>
+		<li>Lugar fisico o web donde se vende productos</li>
+		<li>Tenemos una tabla tienda, en la que guardamos información necesaria para imprimir tickets o para conectarse a la tienda web.</li>
+		</ul>
+	</div>
+	<div>
+		<h2>Diagrama de programa de tienda.php</h2>
+	</div>
+	
+
+</div>
