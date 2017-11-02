40b237dee5ebf9cb930d6bf7d85fb3701ad388c7
Olalla <olalla@superoliva.es>
2017-11-02 20:45:54 +0100

    - comento un poco mas en ayuda de listado tickets cerrados 	modified:   estatico/manualTecnico/help_tickets_cerrados_cobrados/index.php 	modified:   estatico/manualTecnico/help_tickets_cerrados_cobrados/listado_ticketsCerradosCobrados.svg

--- estatico/manualTecnico/help_tickets_cerrados_cobrados/index.php
+++ estatico/manualTecnico/help_tickets_cerrados_cobrados/index.php
@@ -6,6 +6,8 @@
 <p> Objetivo:</p>
 <ul>
 	<li> Ver los tickets cobrados. Pueden cambiar su estado a cerrados, una vez hecho el cierre.</li>
+	<li>Paginacion funciona cuando no hay nada en buscador.</li>
+	<li>Contar Registros solo funciona si no hay nada en buscador y si al buscar el resultado es menor de 40.</li>
 	<li><strong> PENDIENTE:</strong> paginacion al buscar por cliente, num Ticket o forma de pago. Ya que esta alterado nº filtrados, respecto a otras vistas.</li>
 
 	<?php include $DirectorioActual.'./rutas.php' ;?>
