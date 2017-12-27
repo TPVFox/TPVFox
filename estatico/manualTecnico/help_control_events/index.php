3ae7516055af68f32d2a8ef00671c5b36bb4a468
Ricardo Oficina <ricardolau6@gmail.com>
2017-12-27 21:53:55 +0100

    Cambio la ayuda control_events, ya termine para sea realimente un librería.  Changes to be committed: 	modified:   manualTecnico/help_control_events/index.php 	deleted:    manualTecnico/help_control_events/prueba.svg 	deleted:    manualTecnico/help_control_events/rutas.php

--- estatico/manualTecnico/help_control_events/index.php
+++ estatico/manualTecnico/help_control_events/index.php
@@ -4,9 +4,9 @@
 </nav>
 <h2>Controlar los eventos de teclado y raton.</h2>
 <p> El objetivo es hacer un proceso sencillo para poder controlar eventos de tecla y raton. Una idea similar como <a hef="https://github.com/jkup/shortcut">shortcut.js</a>. </p>
-
-<h4>¿Como controlar un input ?</h4>
-<p> Lo que hacemos inicialmente es crear el objeto del input</p>
+<p> Nosotros creamos lib teclado.js, que tenemos en lib/js/, donde una forma sencilla, creo :-), controlamos los eventos de teclado y los eventos de raton</p>
+<h4>¿Como empezar?</h4>
+<p>Lo primero añadiendo es añadir los objetos globales en JS en el head</p>
 <pre>
 <code>
 var idInput = {
@@ -24,7 +24,23 @@ var idInput = {
 
 </code>
 </pre>
-<p>Luego lo que hacemos es en construtor que esta teclado.js crear le obj. </p>
-<p> Luego solo tienes que crear directorio con el contenido, que quieras mostrar.</p>
-<?php include $DirectorioActual.'./rutas.php' ;?>
+<p>Luego añadir la libreria</p>
+<pre>
+<code> &lt;script src="&lt;?php echo $HostNombre; ?&gt;/lib/js/teclado.js"&gt;&lt;/script&gt; </code>
+</pre>
+<p>Poner en input,check,button o la caja permita eventos,<strong>el atributo data-obj="Nombre_objeto_global"</strong></p>
+<p>Tambien poner en el evento queramos controlas llamamos a la funcion controlEventos(event).</p>
+<p>Y ya solo queda currar lo queremos que haga.</p>
+<p>En esta parte tienes que tener en cuenta que las funciones que necesita y utiliza son:</p>
+<ul>
+<li>function controladorAcciones(caja,accion)</li>
+<li>function after_constructor(padre_caja,event)</li>
+<li>function before_constructor(caja)</li>
+</ul>
+
+<h4>¿Que hace la funcion controladorAcciones ?</h4>
+<p>Esta funcion puede ser un switch o simple if donde comprobamos si existe la accion que le tenemos objeto global.</p>
+<p> Llegamos a la funcion cuando pulso una tecla o un evento que tengamos definido en el objeto global, realizar la accion que le indiquemos</p>
+
+
 </div>
