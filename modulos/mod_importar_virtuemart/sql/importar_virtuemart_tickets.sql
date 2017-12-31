5b10ddbc73f51cc38283d65ee71d78e63b072845
Ricardo Oficina <ricardolau6@gmail.com>
2017-12-31 14:49:53 +0100

    Añado tabla para poder registrar los ticketst que restamos stock en web  Changes to be committed: 	new file:   modulos/mod_importar_virtuemart/sql/Leeme.txt 	new file:   modulos/mod_importar_virtuemart/sql/importar_virtuemart_tickets.sql

---
+++ modulos/mod_importar_virtuemart/sql/importar_virtuemart_tickets.sql
@@ -0,0 +1,37 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Dec 31, 2017 at 02:45 PM
+-- Server version: 10.1.26-MariaDB-0+deb9u1
+-- PHP Version: 7.0.19-1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tpv`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `importar_virtuemart_tickets`
+--
+
+CREATE TABLE `importar_virtuemart_tickets` (
+  `idTicketst` int(11) NOT NULL,
+  `Fecha` datetime NOT NULL,
+  `estado` varchar(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
