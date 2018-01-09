ac2020e4d7225796c9c75c9b0b4c556249d7494a
Natalia <nataliaglez92@gmail.com>
2018-01-09 19:13:52 +0100

    Añadido los modulos de compras y ventas

---
+++ modulos/mod_comprasClientes/albaranesListado.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<?php
+include './../../head.php';
+?>
+
+</head>
+
+<body>
+<?php
+
+	include '../../header.php';
+	?>
+	</body>
+</html>
---
+++ modulos/mod_comprasClientes/facturasListado.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<?php
+include './../../head.php';
+?>
+
+</head>
+
+<body>
+<?php
+
+	include '../../header.php';
+	?>
+	</body>
+</html>
+
