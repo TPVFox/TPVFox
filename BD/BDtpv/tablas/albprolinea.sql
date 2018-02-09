3dda926c533e848674b920a6f261949c267c0483
Ricardo Oficina <ricardolau6@gmail.com>
2018-02-09 17:46:43 +0100

    Tablas de albaranes de proveedores.

---
+++ BD/BDtpv/tablas/albprolinea.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Feb 09, 2018 at 05:45 PM
+-- Server version: 10.1.26-MariaDB-0+deb9u1
+-- PHP Version: 7.0.27-0+deb9u1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tpv`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `albprolinea`
+--
+
+CREATE TABLE `albprolinea` (
+  `id` int(11) NOT NULL,
+  `idalbpro` int(11) NOT NULL,
+  `Numalbpro` int(11) NOT NULL,
+  `idArticulo` int(11) NOT NULL,
+  `cref` varchar(18) DEFAULT NULL,
+  `ccodbar` varchar(18) DEFAULT NULL,
+  `cdetalle` varchar(100) DEFAULT NULL,
+  `ncant` decimal(17,6) DEFAULT NULL,
+  `nunidades` decimal(17,6) DEFAULT NULL,
+  `costeSiva` decimal(17,2) DEFAULT NULL,
+  `iva` decimal(4,2) DEFAULT NULL,
+  `nfila` int(11) DEFAULT NULL,
+  `estadoLinea` varchar(12) DEFAULT NULL,
+  `ref_prov` varchar(18) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `albprolinea`
+--
+ALTER TABLE `albprolinea`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `albprolinea`
+--
+ALTER TABLE `albprolinea`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
