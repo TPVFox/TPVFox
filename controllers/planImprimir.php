b5c0bb4a88e5707ef970f5178c48431209cbd2d2
Natalia <nataliaglez92@gmail.com>
2018-02-26 22:12:07 +0100

    Cambio de nombre de TPM a tpm

--- controllers/planImprimir.php
+++ controllers/planImprimir.php
@@ -8,7 +8,7 @@ $pdf->SetMargins(10, 70,10);
 $pdf->setHtmlHeader($cabecera);
 $pdf->AddPage();
 $pdf->writeHTML($html);
-$pdf->Output('/home/olalla/www/prueba.pdf', 'F');
+$pdf->Output($RutaServidor.$rutatmp.'/prueba.pdf', 'F');
 
 
 ?>
