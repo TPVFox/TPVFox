9a32cb16ae0e79cc357ba917c360b429b7c3d6b6
Natalia <nataliaglez92@gmail.com>
2018-03-15 20:10:26 +0100

    Añadidas dos funciones generales a la clase ventas 	modified:   modulos/mod_venta/clases/ClaseVentas.php

--- modulos/mod_venta/clases/ClaseVentas.php
+++ modulos/mod_venta/clases/ClaseVentas.php
@@ -9,6 +9,23 @@ class ClaseVentas{
 		$this->db = $conexion;
 
 	}
+	public function SelectUnResult($tabla, $where){
+		$db=$this->db;
+		$smt=$db->query('SELECT * from '.$tabla.' where '.$where);
+		if ($result = $smt->fetch_assoc () ){
+			$resultado=$result;
+		}
+		return $resultado;
+	}
+	public function SelectVariosResult($tabla, $where){
+		$db=$this->db;
+		$smt=$db->query('SELECT * from '.$tabla.' where '.$where);
+		$resultadoPrincipal=array();
+		while ( $result = $smt->fetch_assoc () ) {
+			array_push($resultadoPrincipal,$result);
+		}
+		return $resultadoPrincipal;
+	}
 }
 
 ?>
