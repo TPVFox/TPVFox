075cc7b92f965699ed316de171965be5a69b0567
Natalia <nataliaglez92@gmail.com>
2018-03-26 22:16:38 +0200

    Tabla nueva de historicos precios 	new file:   BD/BDtpv/tablas/historico_precios.sql

---
+++ BD/BDtpv/tablas/historico_precios.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Mar 26, 2018 at 10:15 PM
+-- Server version: 10.1.26-MariaDB-0+deb9u1
+-- PHP Version: 7.0.27-0+deb9u1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tpv`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `historico_precios`
+--
+
+CREATE TABLE `historico_precios` (
+  `id` int(11) NOT NULL,
+  `idArticulo` int(10) NOT NULL,
+  `Antes` decimal(17,0) NOT NULL,
+  `Nuevo` decimal(17,0) NOT NULL,
+  `Fecha_Creacion` date NOT NULL,
+  `NumDoc` int(11) NOT NULL,
+  `Dedonde` varchar(50) NOT NULL,
+  `Tipo` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `historico_precios`
+--
+ALTER TABLE `historico_precios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `historico_precios`
+--
+ALTER TABLE `historico_precios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
