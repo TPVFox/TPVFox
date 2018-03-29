e1c609e5839293b7e67b4ca7c4f5643e95f6d5ea
Natalia <nataliaglez92@gmail.com>
2018-03-29 12:19:47 +0200

    inicializar el get de recalculo 	modified:   modulos/mod_producto/Recalculo_precios.php

--- modulos/mod_producto/Recalculo_precios.php
+++ modulos/mod_producto/Recalculo_precios.php
@@ -6,12 +6,24 @@
         include './funciones.php';
         include ("./../mod_conexion/conexionBaseDatos.php");
         include ("./../../controllers/Controladores.php");
+        
+        $titulo="Recalculo precios PVP ";
+        if ($_GET['id']){
+			$id=$_GET['id'];
+			$dedonde="albaran";
+			
+			$subtitulo='de '.$dedonde.' :'.$id;
+			$titulo=$titulo.' '.$subtitulo;
+		}
 		?>
 		
 		
 	</head>
 	<body>
 		<script src="<?php echo $HostNombre; ?>/lib/js/teclado.js"></script>
-		
+		<div class="container">
+			<h2 class="text-center"><?php echo $titulo;?></h2>
+			
+		</div>
 	</body>	
 </html>
