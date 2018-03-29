e13678e83ddef60a008d48c63b1164f0ad3976e4
Natalia <nataliaglez92@gmail.com>
2018-03-29 12:06:47 +0200

    plantilla de html inicio 	modified:   modulos/mod_producto/Recalculo_precios.php

---
+++ modulos/mod_producto/Recalculo_precios.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php 
+		
+		include './../../head.php';
+        include './funciones.php';
+        include ("./../mod_conexion/conexionBaseDatos.php");
+        include ("./../../controllers/Controladores.php");
+		?>
+		
+		
+	</head>
+	<body>
+		<script src="<?php echo $HostNombre; ?>/lib/js/teclado.js"></script>
+		
+	</body>	
+</html>
