5a22118b96a0acac94d0b2bad933bb0fc4ac1434
Natalia <nataliaglez92@gmail.com>
2018-04-05 19:19:07 +0200

    Clase tienda creada 	new file:   clases/ClaseTablaTienda.php

---
+++ clases/ClaseTablaTienda.php
@@ -0,0 +1,33 @@
+<?php 
+
+
+class ClaseTablaTienda{
+	
+		public function __construct($conexion){
+		$this->db = $conexion;
+		// Obtenemos el numero registros.
+		$sql = 'SELECT count(*) as num_reg FROM tiendas';
+		$respuesta = $this->consulta($sql);
+		$this->num_rows = $respuesta->fetch_object()->num_reg;
+		// Ahora deberiamos controlar que hay resultado , si no hay debemos generar un error.
+	}
+	public function consulta($sql){
+		$db = $this->db;
+		$smt = $db->query($sql);
+		return $smt;
+	}
+	
+	public function DatosTienda($idTienda){
+		$db = $this->db;
+		$sql='SELECT * FROM tiendas where idTienda='.$idTienda;
+		$smt=$this->consulta($sql);
+		if ($result = $smt->fetch_assoc () ){
+			$tienda=$result;
+		}
+		return $tienda;
+	}
+	
+	
+	
+}
+?>
