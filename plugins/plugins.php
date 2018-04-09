71f438249c4b345e2622cec4ca18e84ef7de21d5
Antonio-Multifrenos <multifrenos.es@gmail.com>
2018-04-09 11:26:49 +0200

    - Añado estructura de plugins.
    	new file:   plugins/mod_producto/fabricantes/parametros.xml
    	new file:   plugins/mod_producto/referencia_cruzadas/parametros.xml
    	modified:   plugins/mod_producto/vehiculos/parametros.xml
    	modified:   plugins/plugins.php

--- plugins/plugins.php
+++ plugins/plugins.php
@@ -3,16 +3,42 @@
  *  y donde se van aplicar.
  *  
  * */
-
-class ClasePlugins {
-	 public $ruta = __DIR__ ;
-	 public $dir; // Directorios y ficheros.
-	 public function __construct($ruta=''){
-		if ($ruta === ''){
+$RutaProyectoCompleta 	= str_replace('plugins','', __DIR__); // Obtenermos la ruta del proyecto
+include ($RutaProyectoCompleta.'/controllers/parametros.php'); 
+class ClasePlugins{
+	 public $ruta 					= '' ;		// (string) ruta completa a plugins
+	 public $RutaServidor 			= '' ;		// (string) ruta del servidor
+	 public $HostNombre				= '' ;		// (string) ruta desde el servidor al proyecto.
+	 public $parametros_plugins		= array(); 	// (array) de varios objetos
+	 
+	public function __construct($modulo=''){
+		// Obtenemos la ruta del proyecto
+		$this->obtenerRutaProyecto();
+		// Obtenermos los directorios ( plugins de ese modulo);
+		if ($modulo === ''){
 			$ruta = $this->ruta;
+		} else {
+			$ruta = $this->ruta.'/'.$modulo;
 		}
-		$this->dir = $this->ObtenerDir($ruta);
+		$this->parametros_plugins = $this->ObtenerPlugins($ruta);
 	}
+	public function ObtenerPlugins($ruta){
+		$respuesta = array();
+		$plugins = $this->ObtenerDir($ruta);
+		$contador = 0;
+		foreach ($plugins as $plugin){
+			// Recorremos los directorios de la ruta indicada.
+			$fichero_parametro = $ruta.'/'.$plugin.'/parametros.xml';
+			// Instanciamos el fichero
+			$parametros = new ClaseParametros($fichero_parametro);
+			$respuesta[$contador]['datos_generales'] =$parametros->ArrayElementos('datos_generales'); 
+			$contador++;
+			
+		} 
+		return $respuesta;
+		
+	}
+	
 	public function ObtenerDir($ruta){
 		// Objetivo scanear directorio y cuales son directorios
 		$respuesta = array();
@@ -20,20 +46,31 @@ class ClasePlugins {
 		foreach ( $scans as $scan){
 			$ruta_completa = $ruta.'/'.$scan;
 			if (filetype($ruta_completa) === 'dir'){
-			 $respuesta[] =$scan;
+				if (($scan === '.') || ($scan === '..')){ 
+					// Descartamos los directorios . y ..
+				} else {	
+					$respuesta[] =$scan;
+				}
 			}
 		}
 		return $respuesta;
-	
 	}
 	
 	
-	public function GetDir(){
+	public function GetParametrosPlugins(){
 		// Objetivo devolver los directorios.
-		return $this->dir;
+		return $this->parametros_plugins;
 	}
 
-
+	public function obtenerRutaProyecto(){
+		// Objectivo
+		// Obtener rutas del servidor y del proyecto.
+		$this->ruta 			=  __DIR__; // Sabemos el directorio donde esta fichero plugins
+		$this->RutaServidor 	= $_SERVER['DOCUMENT_ROOT']; // Sabemos donde esta el servidor.
+		$RutaProyectoCompleta 	= str_replace('plugins','', __DIR__);
+		$this->HostNombre		= str_replace($this->RutaServidor,'',$RutaProyectoCompleta);
+		
+	}
 
 
 
