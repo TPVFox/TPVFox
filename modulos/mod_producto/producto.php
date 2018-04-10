5d14b05abc3ffd552dfaacee9b088f6131431b61
Ricardo Oficina <ricardolau6@gmail.com>
2018-04-11 00:06:50 +0200

    Controlo el POST para poder mostrar lo que hace al usuario.

--- modulos/mod_producto/producto.php
+++ modulos/mod_producto/producto.php
@@ -36,25 +36,62 @@
 		} else {
 			// Quiere decir que no hay id, por lo que es nuevo
 			$titulo .= "Crear";
-
 		}
 		if ($_POST){
 			
 			$preparados= prepararParaGrabar($_POST,$CTArticulos);
-			
 			// Comprobamos los datos antes de grabar.
-			// header('Location: producto.php?id='.$i.'&tipo='.$tipomensaje.'&mensaje='.$mensaje);
+			if (isset($preparados['Sqls']['NuevoProducto'])){
+				// Entonces es que creo uno nuevo.
+				$preparado_nuevo = $preparados['Sqls']['NuevoProducto'];
+				if (isset($preparado_nuevo['insert_articulos']['id_producto_nuevo'])){
+					// Se añadio por lo menos a tabla articulos
+					$id = $preparado_nuevo['insert_articulos']['id_producto_nuevo']; // Asi carga datos.
+					// Montamos comprobaciones para enviar despues de cargar de nuevo producto.
+					$success = array ( 'tipo'=>'success',
+								 'mensaje' =>'Se creo el producto con id '.$id.' nuevo',
+								 'dato' => $preparado_nuevo['consulta']
+								);
+					$preparados['Sqls']['comprobaciones'][] = $success;
+					// Ahora comprobamos si añadio mas cosas en el articulo nuevo. 
+					if (isset($preparado_nuevo['insert_articulos_precios'])){
+						if (isset($preparado_nuevo['insert_articulos_precios']['Afectados'])){
+							// Entiendo que la consulta fue correcta y que se añadio o no.
+							$success = array ( 'tipo'=>'success',
+								 'mensaje' =>'Se añadieron precios correctos en '
+											.$preparado_nuevo['insert_articulos_precios']['Afectados'].' registros',
+								 'dato' => $preparado_nuevo['consulta']
+								);
+							$preparados['Sqls']['comprobaciones'][] = $success;
+						} else {
+							// Hubo un error al insertar los precios.
+							$preparados['Sqls']['comprobaciones'][] = $preparado_nuevo['insert_articulos_precios'];
+						}
+						
+					}
+
+				} else {
+					// Quiere decir que hubo un error al principio
+					$preparados['Sqls']['comprobaciones'][] = $preparado_nuevo;
+				}
+				if (isset($preparado_nuevo['codbarras'])){
+					$preparados['Sqls']['codbarras'] = $preparado_nuevo['codbarras'];
+				}
+			}
 		}
-		//~ echo '<pre>';
-		//~ print_r($Producto);
-		//~ echo '</pre>';
 		// Obtenemos los datos del id, si es 0, quiere decir que es nuevo.
-		$Producto = $CTArticulos->getProducto($_GET['id']);
+		$Producto = $CTArticulos->getProducto($id);
 		if (isset($preparados['Sqls'])){
 			// quiere decir que hizo consultas por lo que tenemos comprobaciones
-			echo 'Algo mucho mas que algo';
-			foreach ($preparados['Sqls']['codbarras'] as $comprobacion){
-				$CTArticulos->SetComprobaciones($comprobacion);
+			if (isset($preparados['Sqls']['comprobaciones'])){
+				foreach ($preparados['Sqls']['comprobaciones'] as $comprobacion){
+					$CTArticulos->SetComprobaciones($comprobacion);
+				}
+			}
+			if (isset($preparados['Sqls']['codbarras'])){
+				foreach ($preparados['Sqls']['codbarras'] as $comprobacion){
+					$CTArticulos->SetComprobaciones($comprobacion);
+				}
 			}
 		}
 		$Producto['comprobaciones'] = $CTArticulos->GetComprobaciones();
