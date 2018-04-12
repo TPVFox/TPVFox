7dfb7f402aa5086f7b7d76ff093e0deecd205873
Natalia <nataliaglez92@gmail.com>
2018-04-12 21:32:52 +0200

    Al imprimir muestra las cantidades con decimales en ventas 	modified:   modulos/mod_venta/funciones.php

--- modulos/mod_venta/funciones.php
+++ modulos/mod_venta/funciones.php
@@ -671,7 +671,7 @@ function montarHTMLimprimir($id , $BDTpv, $dedonde, $tienda){
 			}
 			$imprimir['html'].='<td WIDTH="15%" >'.$producto['cref'].'</td>';
 			$imprimir['html'].='<td WIDTH="40%" >'.$producto['cdetalle'].'</td>';
-			$imprimir['html'].='<td WIDTH="7%" aling="center">'.number_format($producto['nunidades'],0).'</td>';
+			$imprimir['html'].='<td WIDTH="7%" aling="center">'.number_format($producto['nunidades'],2).'</td>';
 			$imprimir['html'].='<td WIDTH="10%" >'.number_format($producto['precioCiva'],2).'</td>';
 			$imprimir['html'].='<td WIDTH="7%" >'.$producto['iva'].'</td>';
 			$importe = $producto['precioCiva']*$producto['nunidades'];
