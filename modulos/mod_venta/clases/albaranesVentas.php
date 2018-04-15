98b41a28b04521215e5787679dcbb2d506efc3ef
nataliaglez <nataliaglez92@gmail.com>
2018-04-16 00:36:03 +0200

    funciones de clase sql en varias lineas de albaranes ventas 	modified:   modulos/mod_venta/clases/albaranesVentas.php

--- modulos/mod_venta/clases/albaranesVentas.php
+++ modulos/mod_venta/clases/albaranesVentas.php
@@ -30,7 +30,10 @@ class AlbaranesVentas extends ClaseVentas{
 		$db = $this->db;
 		$UnicoCampoPedidos=json_encode($pedidos);
 		$UnicoCampoProductos=json_encode($productos);
-		$smt = $db->query ('INSERT INTO albcliltemporales ( idUsuario , idTienda , estadoAlbCli , fechaInicio, idClientes, Pedidos, Productos ) VALUES ('.$idUsuario.' , '.$idTienda.' , "'.$estadoAlbaran.'" , "'.$fecha.'", '.$idCliente.' , '."'".$UnicoCampoPedidos."'".', '."'".$UnicoCampoProductos."'".')');
+		$smt = $db->query ('INSERT INTO albcliltemporales ( idUsuario , idTienda , estadoAlbCli
+		 , fechaInicio, idClientes, Pedidos, Productos ) VALUES ('.$idUsuario.' , '
+		 .$idTienda.' , "'.$estadoAlbaran.'" , "'.$fecha.'", '.$idCliente.' , '
+		 ."'".$UnicoCampoPedidos."'".', '."'".$UnicoCampoProductos."'".')');
 		$id=$db->insert_id;
 		$respuesta['id']=$id;
 		$respuesta['productos']=$productos;
@@ -44,7 +47,10 @@ class AlbaranesVentas extends ClaseVentas{
 		$db = $this->db;
 		$UnicoCampoPedidos=json_encode($pedidos);
 		$UnicoCampoProductos=json_encode($productos);
-		$smt=$db->query('UPDATE albcliltemporales SET idUsuario='.$idUsuario.' , idTienda='.$idTienda.' , estadoAlbCli="'.$estadoAlbaran.'" , fechaInicio='.$fecha.' , Pedidos='."'".$UnicoCampoPedidos."'". ' ,Productos='."'".$UnicoCampoProductos."'".'  WHERE id='.$idTemporal);
+		$smt=$db->query('UPDATE albcliltemporales SET idUsuario='.$idUsuario
+		.' , idTienda='.$idTienda.' , estadoAlbCli="'.$estadoAlbaran.'" , fechaInicio='
+		.$fecha.' , Pedidos='."'".$UnicoCampoPedidos."'". ' ,Productos='."'".$UnicoCampoProductos."'"
+		.'  WHERE id='.$idTemporal);
 		$respuesta['idTemporal']=$idTemporal;
 		$respuesta['productos']=$UnicoCampoProductos;
 	
@@ -56,7 +62,8 @@ class AlbaranesVentas extends ClaseVentas{
 		//SI tenemos un número de albarán real lo metemos en el albarán temporal
 		$db = $this->db;
 		$UnicoCampoPedidos=json_encode($pedidos);
-		$smt=$db->query('UPDATE albcliltemporales SET numalbcli ='.$numAlbaran.' WHERE id='.$idTemporal);
+		$smt=$db->query('UPDATE albcliltemporales SET numalbcli ='.$numAlbaran
+		.' WHERE id='.$idTemporal);
 	}
 	
 	public function buscarDatosAlabaranTemporal($idAlbaranTemporal) {
@@ -100,10 +107,16 @@ class AlbaranesVentas extends ClaseVentas{
 			//Añadir nuevos registros de un albaran real 
 		$db = $this->db;
 		if ($idAlbaran>0){
-		$smt = $db->query ('INSERT INTO albclit (id, Numalbcli, Fecha, idTienda , idUsuario , idCliente , estado , total) VALUES ('.$idAlbaran.' , '.$numAlbaran.', "'.$datos['Fecha'].'", '.$datos['idTienda'].', '.$datos['idUsuario'].', '.$datos['idCliente'].', "'.$datos['estado'].'", '.$datos['total'].')');
+		$smt = $db->query ('INSERT INTO albclit (id, Numalbcli, Fecha, idTienda , 
+		idUsuario , idCliente , estado , total) VALUES ('.$idAlbaran.' , '.$numAlbaran
+		.', "'.$datos['Fecha'].'", '.$datos['idTienda'].', '.$datos['idUsuario'].', '
+		.$datos['idCliente'].', "'.$datos['estado'].'", '.$datos['total'].')');
 		$id=$idAlbaran;
 		}else{
-		$smt = $db->query ('INSERT INTO albclit (Numtemp_albcli, Fecha, idTienda , idUsuario , idCliente , estado , total) VALUES ('.$datos['Numtemp_albcli'].' , "'.$datos['Fecha'].'", '.$datos['idTienda']. ', '.$datos['idUsuario'].', '.$datos['idCliente'].' , "'.$datos['estado'].'", '.$datos['total'].')');
+		$smt = $db->query ('INSERT INTO albclit (Numtemp_albcli, Fecha, idTienda ,
+		 idUsuario , idCliente , estado , total) VALUES ('.$datos['Numtemp_albcli']
+		 .' , "'.$datos['Fecha'].'", '.$datos['idTienda']. ', '.$datos['idUsuario']
+		 .', '.$datos['idCliente'].' , "'.$datos['estado'].'", '.$datos['total'].')');
 		$id=$db->insert_id;
 		$smt = $db->query('UPDATE albclit SET Numalbcli  = '.$id.' WHERE id ='.$id);
 		}
@@ -122,19 +135,33 @@ class AlbaranesVentas extends ClaseVentas{
 				$numPed=0;
 			}
 			if ($idAlbaran>0){
-			$smt=$db->query('INSERT INTO albclilinea (idalbcli  , Numalbcli , idArticulo , cref, ccodbar, cdetalle, ncant, nunidades, precioCiva, iva, nfila, estadoLinea, NumpedCli ) VALUES ('.$id.', '.$idAlbaran.' , '.$prod['idArticulo'].', '."'".$prod['cref']."'".', '.$codBarras.', "'.$prod['cdetalle'].'", '.$prod['ncant'].' , '.$prod['nunidades'].', '.$prod['precioCiva'].' , '.$prod['iva'].', '.$i.', "'. $prod['estadoLinea'].'" , '.$numPed.')' );
+			$smt=$db->query('INSERT INTO albclilinea (idalbcli  , Numalbcli , idArticulo
+			 , cref, ccodbar, cdetalle, ncant, nunidades, precioCiva, iva, nfila, estadoLinea,
+			  NumpedCli ) VALUES ('.$id.', '.$idAlbaran.' , '.$prod['idArticulo'].', '
+			  ."'".$prod['cref']."'".', '.$codBarras.', "'.$prod['cdetalle'].'", '
+			  .$prod['ncant'].' , '.$prod['nunidades'].', '.$prod['precioCiva'].' , '
+			  .$prod['iva'].', '.$i.', "'. $prod['estadoLinea'].'" , '.$numPed.')' );
 			}else{
-			$smt=$db->query('INSERT INTO albclilinea (idalbcli  , Numalbcli , idArticulo , cref, ccodbar, cdetalle, ncant, nunidades, precioCiva, iva, nfila, estadoLinea, NumpedCli ) VALUES ('.$id.', '.$id.' , '.$prod['idArticulo'].', '."'".$prod['cref']."'".', '.$codBarras.', "'.$prod['cdetalle'].'", '.$prod['ncant'].' , '.$prod['nunidades'].', '.$prod['precioCiva'].' , '.$prod['iva'].', '.$i.', "'. $prod['estadoLinea'].'" , '.$numPed.')' );
+			$smt=$db->query('INSERT INTO albclilinea (idalbcli  , Numalbcli , idArticulo
+			 , cref, ccodbar, cdetalle, ncant, nunidades, precioCiva, iva, nfila, 
+			 estadoLinea, NumpedCli ) VALUES ('.$id.', '.$id.' , '.$prod['idArticulo']
+			 .', '."'".$prod['cref']."'".', '.$codBarras.', "'.$prod['cdetalle'].'", '
+			 .$prod['ncant'].' , '.$prod['nunidades'].', '.$prod['precioCiva'].' , '
+			 .$prod['iva'].', '.$i.', "'. $prod['estadoLinea'].'" , '.$numPed.')' );
 			}
 			$i++;
 		}
 		}
 		foreach ($datos['DatosTotales']['desglose'] as  $iva => $basesYivas){
 			if($idAlbaran>0){
-			$smt=$db->query('INSERT INTO albcliIva (idalbcli  ,  Numalbcli  , iva , importeIva, totalbase) VALUES ('.$id.', '.$idAlbaran.' , '.$iva.', '.$basesYivas['iva'].' , '.$basesYivas['base'].')');
+			$smt=$db->query('INSERT INTO albcliIva (idalbcli  ,  Numalbcli  , iva , 
+			importeIva, totalbase) VALUES ('.$id.', '.$idAlbaran.' , '.$iva.', '
+			.$basesYivas['iva'].' , '.$basesYivas['base'].')');
 
 			}else{
-			$smt=$db->query('INSERT INTO albcliIva (idalbcli  ,  Numalbcli  , iva , importeIva, totalbase) VALUES ('.$id.', '.$id.' , '.$iva.', '.$basesYivas['iva'].' , '.$basesYivas['base'].')');
+			$smt=$db->query('INSERT INTO albcliIva (idalbcli  ,  Numalbcli  , iva ,
+			 importeIva, totalbase) VALUES ('.$id.', '.$id.' , '.$iva.', '
+			 .$basesYivas['iva'].' , '.$basesYivas['base'].')');
 
 			}
 		}
@@ -142,10 +169,14 @@ class AlbaranesVentas extends ClaseVentas{
 		foreach ($pedidos as $pedido){
 			if ($pedido['estado']=="activo" || $pedido['estado']=="Activo"){
 			if($idAlbaran>0){
-				$smt=$db->query('INSERT INTO pedcliAlb (idAlbaran  ,  numAlbaran   , idPedido , numPedido) VALUES ('.$id.', '.$idAlbaran.' ,  '.$pedido['idPedCli'].' , '.$pedido['Numpedcli'].')');
+				$smt=$db->query('INSERT INTO pedcliAlb (idAlbaran  ,  numAlbaran  
+				 , idPedido , numPedido) VALUES ('.$id.', '.$idAlbaran.' ,  '
+				 .$pedido['idPedCli'].' , '.$pedido['Numpedcli'].')');
 
 				}else{
-				$smt=$db->query('INSERT INTO pedcliAlb (idAlbaran  ,  numAlbaran   , idPedido , numPedido) VALUES ('.$id.', '.$id.' ,  '.$pedido['idPedCli'].' , '.$pedido['Numpedcli'].')');
+				$smt=$db->query('INSERT INTO pedcliAlb (idAlbaran  ,  numAlbaran  
+				 , idPedido , numPedido) VALUES ('.$id.', '.$id.' ,  '.$pedido['idPedCli']
+				 .' , '.$pedido['Numpedcli'].')');
 				}
 			}
 		}
@@ -167,7 +198,8 @@ class AlbaranesVentas extends ClaseVentas{
 		//@Objetivo:
 		//Mostrar algunos datos de todos los albaranes reales con un filtro
 		$db=$this->db;
-		$smt=$db->query('SELECT a.id , a.Numalbcli , a.Fecha , b.Nombre, a.total, a.estado FROM `albclit` as a LEFT JOIN clientes as b on a.idCliente=b.idClientes '.$filtro);
+		$smt=$db->query('SELECT a.id , a.Numalbcli , a.Fecha , b.Nombre, a.total,
+		 a.estado FROM `albclit` as a LEFT JOIN clientes as b on a.idCliente=b.idClientes '.$filtro);
 		$albaranesPrincipal=array();
 		while ( $result = $smt->fetch_assoc () ) {
 			array_push($albaranesPrincipal,$result);
@@ -179,7 +211,8 @@ class AlbaranesVentas extends ClaseVentas{
 		//@Objetivo:
 		//Mostrar la suma de los impirtes ivas y total base   de un albaran real
 		$db=$this->db;
-		$smt=$db->query('select sum(importeIva ) as importeIva , sum(totalbase) as  totalbase from albcliIva where  Numalbcli  ='.$numAlbaran);
+		$smt=$db->query('select sum(importeIva ) as importeIva , sum(totalbase)
+		 as  totalbase from albcliIva where  Numalbcli  ='.$numAlbaran);
 		if ($result = $smt->fetch_assoc () ){
 			$albaran=$result;
 		}
@@ -190,7 +223,9 @@ class AlbaranesVentas extends ClaseVentas{
 			//@Objetivo:
 			//Mostrar todos los datos temporales
 			$db = $this->db;
-			$smt = $db->query ('SELECT tem.numalbcli, tem.id , tem.idClientes, tem.total, b.Nombre from albcliltemporales as tem left JOIN clientes as b on tem.idClientes=b.idClientes');
+			$smt = $db->query ('SELECT tem.numalbcli, tem.id , tem.idClientes,
+			 tem.total, b.Nombre from albcliltemporales as tem left JOIN clientes
+			  as b on tem.idClientes=b.idClientes');
 			$albaranPrincipal=array();
 			while ( $result = $smt->fetch_assoc () ) {
 				array_push($albaranPrincipal,$result);
@@ -271,7 +306,8 @@ class AlbaranesVentas extends ClaseVentas{
 		$db=$this->db;
 		$pedido['busqueda']=$busqueda;
 		if ($busqueda>0){
-		$smt=$db->query('select  Numalbcli , id , Fecha  , total from albclit where Numalbcli ='.$busqueda.' and  idCliente='. $idCliente);
+		$smt=$db->query('select  Numalbcli , id , Fecha  , total from albclit where
+		 Numalbcli ='.$busqueda.' and  idCliente='. $idCliente);
 		if ($smt){
 			if ($result = $smt->fetch_assoc () ){
 				$pedido=$result;
@@ -279,7 +315,8 @@ class AlbaranesVentas extends ClaseVentas{
 			$pedido['Nitem']=1;
 		}
 		}else{
-			$smt=$db->query('SELECT  Numalbcli , Fecha  , total , id from albclit where idCliente='.$idCliente .' and estado="Guardado"');
+			$smt=$db->query('SELECT  Numalbcli , Fecha  , total , id from albclit 
+			where idCliente='.$idCliente .' and estado="Guardado"');
 		
 			$pedidosPrincipal=array();
 			while ( $result = $smt->fetch_assoc () ) {
