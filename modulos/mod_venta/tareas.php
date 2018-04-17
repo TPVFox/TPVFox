64624ae4e98cf7906006e2a53304725cf03a5a10
nataliaglez <nataliaglez92@gmail.com>
2018-04-17 22:51:39 +0200

    Mostrar errores de sql en comprobarPedidos 	modified:   modulos/mod_venta/tareas.php

--- modulos/mod_venta/tareas.php
+++ modulos/mod_venta/tareas.php
@@ -382,20 +382,26 @@ switch ($pulsado) {
 		//Comprobar los pedidos en estado guardado que son de un cliente
 			$idCliente=$_POST['idCliente'];
 			$estado="Guardado";
+			$respuesta=array();
 			if ($idCliente>0){
 				$comprobar=$CcliPed->ComprobarPedidos($idCliente, $estado);
-				if (isset ($comprobar['ped'])){
-					if ($comprobar['ped']==1){
-						$respuesta['ped']=1;
-					
+				if(isset($comprobar['error'])){
+					$respuesta['error']=$comprobar['error'];
+					$respuesta['consulta']=$comprobar['consulta'];
+				}else{
+					if (isset ($comprobar['ped'])){
+						if ($comprobar['ped']==1){
+							$respuesta['ped']=1;
+						
+						}else{
+							$respuesta['ped']=0;
+						}
 					}else{
 						$respuesta['ped']=0;
 					}
-				}else{
-					$respuesta['ped']=0;
-				}
 				
 			}
+		}
 			echo json_encode($respuesta);
 		break;
 		
