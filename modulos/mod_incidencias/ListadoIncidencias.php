554b398deb75671828b41dd9b741f5fa419d3c96
Natalia <nataliaglez92@gmail.com>
2018-04-23 18:01:47 +0200

    Plantilla incidencias 	modified:   modulos/mod_incidencias/ListadoIncidencias.php

---
+++ modulos/mod_incidencias/ListadoIncidencias.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php 
+	include './../../head.php';
+	include './funciones.php';
+	include ("./../../plugins/paginacion/paginacion.php");
+	include ("./../../controllers/Controladores.php");
+	
+	?>
+</head>
+<body>
+	<script src="<?php echo $HostNombre; ?>/modulos/mod_incidencias/funciones.js"></script>
+    <script src="<?php echo $HostNombre; ?>/controllers/global.js"></script>    
+     <?php
+
+	include '../../header.php';
+	?>
+		<div class="container">
+		<div class="row">
+			<div class="col-md-12 text-center">
+					<h2> Listado de incidencias </h2>
+				</div>
+</body>
+</html>
