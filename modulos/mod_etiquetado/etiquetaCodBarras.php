fa8b9383c89715201a9b2d4823e2981f9b894ea2
Natalia <nataliaglez92@gmail.com>
2018-05-02 18:12:33 +0200

    Crear plantilla de etiquetasCodBarras 	modified:   modulos/mod_etiquetado/etiquetaCodBarras.php

---
+++ modulos/mod_etiquetado/etiquetaCodBarras.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<?php
+        include './../../head.php';
+        include './funciones.php';
+        include ("./../mod_conexion/conexionBaseDatos.php");
+        
+        $titulo="Crear Etiquetas de Código Barras";
+        ?>
+     </head>
+	<body>
+	<?php     
+        include './../../header.php';
+        ?>
+        <div class="container">
+		<h2 class="text-center"> <?php echo $titulo;?></h2>
+		</div>
+	</body>
+</html>
+      
