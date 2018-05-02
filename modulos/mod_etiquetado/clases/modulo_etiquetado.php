90e5b072ca09ff7934fd7333849e3df4b306375c
Natalia <nataliaglez92@gmail.com>
2018-05-02 18:35:20 +0200

    Preparación de la clase etiquetado 	new file:   modulos/mod_etiquetado/clases/modulo_etiquetado.php

---
+++ modulos/mod_etiquetado/clases/modulo_etiquetado.php
@@ -0,0 +1,29 @@
+<?php 
+
+class Modulo_etiquetado{
+	public function consulta($sql){
+		// Realizamos la consulta.
+		// Esta consulta no tiene sentido teniendo la del padre...
+		
+		$db = $this->db;
+		$smt = $db->query($sql);
+		if ($smt) {
+			return $smt;
+		} else {
+			$respuesta = array();
+			$respuesta['consulta'] = $sql;
+			$respuesta['error'] = $db->error;
+			return $respuesta;
+		}
+	}
+	public function __construct($conexion){
+		$this->db = $conexion;
+		// Obtenemos el numero registros.
+		$sql = 'SELECT count(*) as num_reg FROM modulo_etiquetado';
+		$respuesta = $this->consulta($sql);
+		$this->num_rows = $respuesta->fetch_object()->num_reg;
+		// Ahora deberiamos controlar que hay resultado , si no hay debemos generar un error.
+	}
+}
+
+?>
