9357cb479b9ddce19e6109adeebf13c52c220f3c
nataliaglez <nataliaglez92@gmail.com>
2018-05-13 13:54:37 +0200

    añadir en tareas comprobar pedidos y albaranes 	modified:   modulos/mod_venta/tareas.php 	new file:   modulos/mod_venta/tareas/comprobarAlbaranes.php 	new file:   modulos/mod_venta/tareas/comprobarPedidos.php

--- modulos/mod_venta/tareas.php
+++ modulos/mod_venta/tareas.php
@@ -64,58 +64,10 @@ switch ($pulsado) {
 		break;
 		
 		case 'comprobarPedidos':
-		//Objetivo:
-		//Comprobar los pedidos en estado guardado que son de un cliente
-			$idCliente=$_POST['idCliente'];
-			$estado="Guardado";
-			$respuesta=array();
-			if ($idCliente>0){
-				$comprobar=$CcliPed->ComprobarPedidos($idCliente, $estado);
-				if(isset($comprobar['error'])){
-					$respuesta['error']=$comprobar['error'];
-					$respuesta['consulta']=$comprobar['consulta'];
-				}else{
-					if (isset ($comprobar['ped'])){
-						if ($comprobar['ped']==1){
-							$respuesta['ped']=1;
-						
-						}else{
-							$respuesta['ped']=0;
-						}
-					}else{
-						$respuesta['ped']=0;
-					}
-				
-			}
-		}
+			include 'tareas/comprobarPedidos.php';
 		break;
-		
 		case 'comprobarAlbaran':
-		//Objetivo:
-		//Comprobar los albaranes con estado guardado que son del cliente seleccionado
-		$idCliente=$_POST['idCliente'];
-		$estado="Guardado";
-		$respuesta=array();
-			if ($idCliente>0){
-				$comprobar=$CalbAl->ComprobarAlbaranes($idCliente, $estado);
-				if (isset($comprobar['error'])){
-					$respuesta['error']=$comprobar['error'];
-					$respuesta['consulta']=$comprobar['consulta'];
-				}else{
-					if (isset ($comprobar['alb'])){
-						if ($comprobar['alb']==1){
-							$respuesta['alb']=1;
-							
-						}else{
-							$respuesta['alb']=0;
-							
-						}	
-					}else{
-						$respuesta['alb']=0;
-						$respuesta['sql']=$comprobar['sql'];
-					}
-				}
-			}
+			include 'tareas/comprobarAlbaranes.php';
 		break;
 		
 		
