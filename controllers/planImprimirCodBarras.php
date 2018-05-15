4947ed528f5922261faeb8319dabdc0382ac06c4
Natalia <nataliaglez92@gmail.com>
2018-05-15 19:47:37 +0200

    Maquetando etiquetas 	modified:   controllers/planImprimirCodBarras.php

--- controllers/planImprimirCodBarras.php
+++ controllers/planImprimirCodBarras.php
@@ -4,12 +4,12 @@
 
 $pdf = new imprimir(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 $cabecera="";
-$pdf->SetMargins(0, 10,0, true);
+$pdf->SetMargins(0, 20,0, true);
 $pdf->setHtmlHeader($cabecera);
 $pdf->AddPage();
 $style = array(
     'position' => '',
-    'align' => 'C',
+    'align' => 'L',
     'stretch' => false,
     'fitwidth' => true,
     'cellfitalign' => '',
@@ -25,8 +25,9 @@ $style = array(
 );
 
 
-$style['cellfitalign'] = 'C';
+$style['cellfitalign'] = 'L';
 $i=0;
+
 foreach($lotes as $lote){
 	$etiquetas=$CEtiquetado->datosLote($lote);
 	$productos=$etiquetas['productos'];
@@ -35,17 +36,18 @@ foreach($lotes as $lote){
 		if($i==2){
 			$i=0;
 		}
-		
-		
-	
 			$x = $pdf->GetX();
             $y = $pdf->GetY();
+           
+           $texto=$producto['nombre']." Peso:".$producto['peso']." Precio".$producto['precio'];
             //~ $pdf->write1DBarcode($producto['codBarras'], 'EAN13', '', $y-8.5, 105, 18, 0.4, $style, 'M');
-            $pdf->write1DBarcode($producto['codBarras'], 'EAN13', '', $y, 105, 18, 0.4, $style, 'M');
-            //Reset X,Y so wrapping cell wraps around the barcode's cell.
+            $pdf->write1DBarcode($producto['codBarras'], 'EAN13', '', $y-10, 105, 18, 0.4, $style, 'M');
             $pdf->SetXY($x,$y);
+			$pdf->Cell(105, 30, $texto, 1, 0, 'C', FALSE, '', 0, FALSE, 'C', 'B');
+			
+				
             //~ $pdf->Cell(105, 51, $producto['nombre'], 1, 0, 'C', FALSE, '', 0, FALSE, 'C', 'B');
-            $pdf->Cell(105, 51, $producto['nombre'], 1, 0, 'C', FALSE, '', 0, FALSE, 'C', 'B');
+            
     
 		if($i==1){
 			$pdf->Ln();
@@ -55,9 +57,5 @@ foreach($lotes as $lote){
 		
 	}
 }
-//~ $pdf->writeHTML($html);
-//~ $pdf->write1DBarcode('8410014820938', 'EAN13', '', '', '', 18, 0.4, '', 'N');
-//~ $pdf->Output($RutaServidor.$rutatmp.'/'.$nombreTmp, 'F');
-//~ $pdf->Output('etiquetas.pdf', 'I');
 $pdf->Output($RutaServidor.$rutatmp.'/'.$nombreTmp, 'F');
 ?>
