4cb6ffc0183a228ca889efb1b4fcb01627b4313d
Natalia <nataliaglez92@gmail.com>
2018-05-16 17:12:23 +0200

    Maquetados etiquetas posición 	modified:   controllers/planImprimirCodBarras.php

--- controllers/planImprimirCodBarras.php
+++ controllers/planImprimirCodBarras.php
@@ -4,7 +4,7 @@
 
 $pdf = new imprimir(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 $cabecera="";
-$pdf->SetMargins(0, 10,0, true);
+$pdf->SetMargins(3, 10,3, false);
 $pdf->setHtmlHeader($cabecera);
 $pdf->AddPage();
 $style = array(
@@ -13,7 +13,7 @@ $style = array(
     'stretch' => false,
     'fitwidth' => true,
     'cellfitalign' => '',
-    'border' => true,
+    'border' => false,
     'hpadding' => 'auto',
     'vpadding' => 'auto',
     'fgcolor' => array(0,0,0),
@@ -27,6 +27,7 @@ $style = array(
 
 $style['cellfitalign'] = 'L';
 $i=0;
+$cont=0;
 $pdf->SetFont('helvetica', '', 9);
 foreach($lotes as $lote){
 	$etiquetas=$CEtiquetado->datosLote($lote);
@@ -39,17 +40,21 @@ foreach($lotes as $lote){
 			$x = $pdf->GetX();
             $y = $pdf->GetY();
            
-           $texto1='Lote: '.$lote.' Fecha Env: '.$etiquetas['fecha_env'];
-           $texto2=$producto['nombre'].'<br> '.'Fecha cad: '.$etiquetas['fecha_cad'].' <br> Precio: '.$producto['precio'];
-            $pdf->write1DBarcode($producto['codBarras'], 'EAN13', '', $y, 105, 18, 0.4, $style, 'M');
+           $texto1='Lote: '.$lote.'  Fecha Env: '.$etiquetas['fecha_env'];
+           $texto2='<br><br>'.$producto['nombre'].'<br> '.'Fecha cad: '.$etiquetas['fecha_cad'].' <br> Precio: '.$producto['precio'];
+            $pdf->write1DBarcode($producto['codBarras'], 'EAN13', '', $y+3, 105, 18, 0.4, $style, 'M');
             $pdf->SetXY($x,$y);
-			$pdf->MultiCell(55, 30, $texto1, 1, 'L', 0, 0, '', '', true, 0, false, true, 45 ,'M');
-			$pdf->MultiCell(50, 30, $texto2, 1, 'L', 0, 0, '', '', true, 0, true, true, 45 ,'M');
+			$pdf->MultiCell(55, 34.3, $texto1, 0, 'L', 0, 0, '', '', true, 0, false, true, 45 ,'M');
+			$pdf->MultiCell(50, 34.3, $texto2, 0, 'L', 0, 0, '', '', true, 0, true, true, 45 ,'M');
 		if($i==1){
 			$pdf->Ln();
 		}
-		
+		$cont++;
 		$i++;
+		if($cont==16){
+			$pdf->AddPage();
+			$cont=0;
+		}
 		
 	}
 }
