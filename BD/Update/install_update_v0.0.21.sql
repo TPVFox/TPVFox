adf8fb96da0aa943e6637867cea482a292d7f486
Ricardo Oficina <ricardolau6@gmail.com>
2018-05-27 13:18:15 +0200

    Este cambio ya se aplico pero falta ver que se hace con los registros anteriores.

--- BD/Update/install_update_v0.0.21.sql
+++ BD/Update/install_update_v0.0.21.sql
@@ -6,7 +6,8 @@ ALTER TABLE `albclilinea` ADD `pvpSiva` DECIMAL(17.6) NULL AFTER `NumpedCli`;
 ALTER TABLE `facclilinea` ADD `pvpSiva` DECIMAL(17.6) NULL AFTER `NumalbCli`;
 
 
-
-update pedclilinea SET pvpSiva = precioCiva / (1+iva/100);
-update albclilinea SET pvpSiva = precioCiva / (1+iva/100);
-update facclilinea SET pvpSiva = precioCiva / (1+iva/100);
+-- Queda pendiente cubrir el campo de registros anteriores
+-- podría cubrir todos los campos con esto, pero podría provocar que no coincida los totales.
+-- update pedclilinea SET pvpSiva = precioCiva / (1+iva/100);
+-- update albclilinea SET pvpSiva = precioCiva / (1+iva/100);
+-- update facclilinea SET pvpSiva = precioCiva / (1+iva/100);
