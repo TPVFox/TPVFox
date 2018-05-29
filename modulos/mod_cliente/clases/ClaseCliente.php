28ec25bcc608844571ef1fe2a9c2960a7fee4e1a
nataliaglez <nataliaglez92@gmail.com>
2018-05-29 12:09:21 +0200

    Comentarios en la clase de clientes 	modified:   modulos/mod_cliente/clases/ClaseCliente.php

--- modulos/mod_cliente/clases/ClaseCliente.php
+++ modulos/mod_cliente/clases/ClaseCliente.php
@@ -7,26 +7,49 @@ include_once $RutaServidor . $HostNombre . '/modulos/claseModelo.php';
 class ClaseCliente extends modelo{
 	
 	public function getCliente($id){
+		//@Objetivo:
+		//Cargar todos los datos de un cliente
+		//@Parametros: 
+		//id => recibe el id del cliente que queremos buscar
 		$sql= 'SELECT * FROM clientes WHERE idClientes='.$id;
 		return $this->consulta($sql);
 	}
 	public function getTicket($id){
+		//@Objetivo: Cargar todos los tickes de un cliente determinado en orden descendente
+		//@Parametros:
+		//id -> id del cliente
 		$sql='SELECT Numticket as num , Fecha as fecha , total ,id FROM ticketst WHERE idCliente= '.$id.' order by Numticket desc';
 		return $this->consulta($sql);
 	}
 	public function getFacturas($id){
+		//@Objetivo:
+		//Cargar todas las facturas de un cliente detrerminado en orden descendente
+		//@Parametros:
+		//id -> id del cliente
 		$sql='SELECT Numfaccli as num, Fecha as fecha, total, id FROM facclit WHERE idCliente='.$id.' order by id desc';
 		return $this->consulta($sql);
 	}
 	public function getAlbaranes($id){
+		//@objetivo:
+		//Cargar todos los albaranes de clientes de un cliente determinado en ordes descendente
+		//@Parametros:
+		//id->id del cliente
 		$sql='SELECT Numalbcli as  num, Fecha as fecha, total, id FROM albclit WHERE idCliente='.$id.' order by id desc';
 		return $this->consulta($sql);
 	}
 	public function getPedidos($id){
+		//@Objetivo:
+		//Cargar todos los pedidos de clientes de un cliente determinado en orden descendente
+		//@Parametros:
+		//id-> id del cliente
 		$sql='SELECT Numpedcli as num, FechaPedido as fecha, total, id FROM pedclit WHERE idCliente='.$id.' order by id desc';
 		return $this->consulta($sql);
 	}
 	public function adjuntosCliente($id){
+		//@Objetivo: 
+		//Cargar todos los adjunto de un cliente , tickets, facturas, albaranes y pedidos
+		//@Parametros:
+		//id-> id del cliente 
 		$respuesta=array();
 		$respuesta['tickets']=$this->getTicket($id);
 		$respuesta['facturas']=$this->getFacturas($id);
@@ -35,17 +58,25 @@ class ClaseCliente extends modelo{
 		return $respuesta;
 	}
 	public function modificarDatosCliente($datos, $id){
+		//@Objetivo:
+		//Modificar los datos de un cliente determinado
+		//@Parametros:
+		//Datos-> array con todos los datos del cliente
+		//id-> id del cliente que se va a modificar
 		$sql='UPDATE `clientes` SET Nombre="'.$datos['nombre'].'" , razonsocial="'.$datos['razonsocial'].'" , 
 		nif="'.$datos['nif'].'" , direccion="'.$datos['direccion'].'" , codpostal="'.$datos['codpostal'].'" , telefono="'.$datos['telefono']
 		.'" , movil="'.$datos['movil'].'" , fax="'.$datos['fax'].'" , email="'.$datos['email'].'" , estado="'.$datos['estado'].'" ,
 		fomasVenci='."'".$datos['formasVenci']."'".' WHERE idClientes='.$id;
-		//~ $consulta=$this->consulta($sql);
 		$consulta=$this->consultaDML($sql);
 		if(isset($consulta['error'])){
 			return $consulta;
 		}
 	}
 	public function addcliente($datos){
+		//@Objetivo:
+		//Añadir un cliente nuevo
+		//@Parametros:
+		//datos-> todos los datos que se recogen de la ficha de clientes 
 		$sql='INSERT INTO `clientes`( `Nombre`, `razonsocial`, 
 		`nif`, `direccion`, `codpostal`, `telefono`, `movil`, `fax`, `email`, 
 		`estado`, `fomasVenci`, `fecha_creado`) VALUES ("'.$datos['nombre'].'", "'.$datos['razonsocial'].'", 
