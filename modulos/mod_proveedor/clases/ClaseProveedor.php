b8dfb1514d7e9c34cbfd92f66f7e293ad367f104
Natalia <nataliaglez92@gmail.com>
2018-05-29 18:14:40 +0200

    Funciones en la clase para buscar datos principales de albaranes, facturas y pedidos 	modified:   modulos/mod_proveedor/clases/ClaseProveedor.php

--- modulos/mod_proveedor/clases/ClaseProveedor.php
+++ modulos/mod_proveedor/clases/ClaseProveedor.php
@@ -9,7 +9,20 @@ class ClaseProveedor extends modelo{
 		return $this->consulta($sql);
 	}
 	
+	public function getFacturas($id){
+		$sql='SELECT Numfacpro  as num, Fecha as fecha, total, id FROM facprot WHERE idProveedor='.$id;
+		return $this->consulta($sql);
+	}
 	
+	public function getAlbaranes($id){
+		$sql='SELECT Numalbpro as num. Fecha as fecha, total , id FROM albprot WHERE idProveedor='.$id;
+		return $this->consulta($sql);
+	}
+	
+	public function getPedidos($id){
+		$sql='SELECT Numpedpro as num, FechaPedido as fecha , total, id FROM pedprot WHERE idProveedor='.$id;
+		return $this->consulta($sql);
+	}
 	
 }
 
