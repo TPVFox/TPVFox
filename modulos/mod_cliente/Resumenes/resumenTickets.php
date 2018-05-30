c2f7eb362c52d547db047f1193dcd9a650f67d3d
Natalia <nataliaglez92@gmail.com>
2018-05-30 17:54:57 +0200

    Comprobar que los datos llegan bien 	modified:   modulos/mod_cliente/Resumenes/resumenTickets.php 	modified:   modulos/mod_cliente/cliente.php

--- modulos/mod_cliente/Resumenes/resumenTickets.php
+++ modulos/mod_cliente/Resumenes/resumenTickets.php
@@ -10,8 +10,22 @@
         $ClasesParametros = new ClaseParametros('../parametros.xml');  
         $Controler = new ControladorComun; 
 		$Controler->loadDbtpv($BDTpv);
+		$errores=array();
+		if(isset($_GET['fechaIni']) & isset($_GET['fechaFin'])){
+			$fechaIni=$_GET['fechaIni'];
+			$fechaFin=$_GET['fechaFin'];
+			$idCliente=$_GET['idCliente'];
+			echo $fechaIni;
+			echo $fechaFin;
+		}else{
+			$errores[1]=array ( 'tipo'=>'DANGER!',
+								 'dato' => '',
+								 'class'=>'alert alert-danger',
+								 'mensaje' => 'Error no se han enviado corectamente las fechas'
+								 );
+		}
 		?>
-			</head>
+	</head>
 	<body>
 		<script src="<?php echo $HostNombre; ?>/modulos/mod_cliente/funciones.js"></script>
 		<script src="<?php echo $HostNombre; ?>/modulos/mod_incidencias/funciones.js"></script>
@@ -20,7 +34,7 @@
 		?>
 		<div class="container">
 			
-			</div>
+		</div>
 		<?php 
 echo '<script src="'.$HostNombre.'/plugins/modal/func_modal.js"></script>';
 include $RutaServidor.'/'.$HostNombre.'/plugins/modal/busquedaModal.php';
