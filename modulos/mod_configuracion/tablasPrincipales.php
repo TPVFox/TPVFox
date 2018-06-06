d04e5a1fb17c63cd4f7225ddbad78207509d3fc8
Natalia <nataliaglez92@gmail.com>
2018-06-06 19:23:25 +0200

    Mostrar la tabla de ivas 	new file:   modulos/mod_configuracion/clases/ClaseIva.php 	modified:   modulos/mod_configuracion/tablasPrincipales.php

--- modulos/mod_configuracion/tablasPrincipales.php
+++ modulos/mod_configuracion/tablasPrincipales.php
@@ -3,7 +3,9 @@
     <head>
 <?php 
 include './../../head.php';
-
+include_once $RutaServidor . $HostNombre .'/modulos/mod_configuracion/clases/ClaseIva.php';
+$iva=new ClaseIva($BDTpv);
+$todosIvas=$iva->cargarDatos();
 
 
 
@@ -27,9 +29,25 @@ include './../../head.php';
 							<tr>
 								<th>ID</th>
 								<th>Descripción</th>
+								<th>Iva</th>
+								<th>Recargo</th>
 								<th></th>
 							</tr>
 						</thead>
+						<tbody>
+							<?php 
+							foreach($todosIvas['datos'] as $iva){
+								echo '<tr>';
+								echo '<td>'.$iva['idIva'].'</td>';
+								echo '<td>'.$iva['descripcionIva'].'</td>';
+								echo '<td>'.$iva['iva'].'</td>';
+								echo '<td>'.$iva['recargo'].'</td>';
+								echo '<td></td>';
+								echo '</tr>';
+							}
+							
+							?>
+						</tbody>
 					</table>
 			</div>
 			<div class="col-md-4  text-center">
