5e8ba4fbc7039520fff772a5c5d090e65d9303b0
Natalia <nataliaglez92@gmail.com>
2018-06-07 20:17:21 +0200

    Comentarios en clase de tarifas 	modified:   modulos/mod_cliente/clases/claseTarifaCliente.php

--- modulos/mod_cliente/clases/claseTarifaCliente.php
+++ modulos/mod_cliente/clases/claseTarifaCliente.php
@@ -14,6 +14,9 @@ include_once $RutaServidor.$HostNombre.'/modulos/claseModelo.php';
 class TarifaCliente extends modelo {
 
     public function leer($idcliente) {
+		//Objetivo: buscar todos los datos de las tarifas que tiene un cliente
+		//Parametros: 
+		//-idCliente: id del cliente
         $sql = 'SELECT artcli.*'
                 . ', art.iva as ivaArticulo '
                 . ', art.articulo_name as descripcion '
@@ -25,6 +28,10 @@ class TarifaCliente extends modelo {
     }
 
     public function leerPrecio($idcliente, $idarticulo) {
+		//Objetivo: leer los datos de un articulo de las tarifas de los clientes
+		//Parametros:
+		//-idcliente: id del cliente
+		//-idarticulo: id del articulo
         $sql = 'SELECT artcli.*'
                 . ', art.iva as ivaArticulo '
                 . ', art.articulo_name as descripcion '
@@ -36,6 +43,10 @@ class TarifaCliente extends modelo {
     }
 
     public function existeArticulo($idcliente, $idarticulo) {
+		//Objetivo: comprobas que un cliente tiene tarifa con un articulo determinado
+		//Parametros:
+		//-idcliente: id del cliente
+		//-idarticulo: id del articulo
         $sql = 'SELECT count(*) as contador '
                 . 'FROM articulosClientes AS artcli'
                 . ' WHERE artcli.idClientes=' . $idcliente
@@ -52,6 +63,11 @@ class TarifaCliente extends modelo {
     }
 
     public function update($idcliente, $idarticulo, $modificaciones) {
+		//@Objetivo: modificar los datos de una tarifa
+		//@Parametros: 
+		//idcliente: id del cliente
+		//idarticulo: id del articulo al que se le va a modificar la tarifa
+		//modificaciones: array con las modificaciones a la tarifa
 
         $updateStr = [];
         if (is_array($modificaciones)) {
@@ -73,6 +89,9 @@ class TarifaCliente extends modelo {
     }
 
     public function insert($datos) {
+		//@Objetivo: insertar una nueva tarifa
+		//@Parametros:
+		//Datos: array de datos, con todos los datos de la tarifa nueva
 
         $updateStr = [];
         if (is_array($datos)) {
@@ -93,7 +112,7 @@ class TarifaCliente extends modelo {
     }
 
     public function replace($idcliente, $idarticulo, $datos) {
-
+//queda esto por comentar
         $updateStr = [];
         $updateStr[] = ' idArticulo= ' . $idarticulo;
         $updateStr[] = ' idClientes= ' . $idcliente;
