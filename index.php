4118790a2a1014613a9f65bf0459d4b2e919f488
Natalia <nataliaglez92@gmail.com>
2018-06-11 21:28:55 +0200

    Iconos en la página principal 	new file:   css/img/attention-3038612.svg 	new file:   css/img/database-schema-1895779.svg 	new file:   css/img/desk-44076.svg 	new file:   css/img/document-2034327.svg 	new file:   css/img/folder-21035081.svg 	new file:   css/img/folder-236091.svg 	new file:   css/img/handshake-651818.svg 	modified:   css/img/help.txt 	new file:   css/img/list-1479042.svg 	new file:   css/img/pen-1586721.svg 	new file:   css/img/tienda2.svg modified:   css/img/van-19185551.ai.svg 	modified:   index.php

--- index.php
+++ index.php
@@ -49,12 +49,15 @@
 						<h4 class="text-center">Compras</h4>
 						<div class="col-md-4 text-center">
 								<h6 class="text-center">Pedidos</h6>
+								<a class="text-center" title="Pedidos" href="modulos/mod_compras/pedidosListado.php"><img width="35px" src="css/img/list-1479042.svg" alt="Pedidos" /></a>
 							</div>
 							<div class="col-md-4 text-center">
 								<h6 class="text-center">Albaranes</h6>
+								<a class="text-center" title="Albaran" href="modulos/mod_compras/albaranesListado.php"><img width="45px" src="css/img/desk-44076.svg" alt="Albaran" /></a>
 							</div>
 							<div class="col-md-4 text-center">
 								<h6 class="text-center">Facturas</h6>
+								<a class="text-center" title="Factura" href="modulos/mod_compras/facturasListado.php"><img width="25px" src="css/img/pen-1586721.svg" alt="Factura" /></a>
 							</div>
 						</div>
 						
@@ -62,14 +65,17 @@
 					<div class="col-md-4" >
 						<div class="col-md-11" style="margin: 4%;padding:3%;border-radius:10px;background-color:#f3f3f6;">
 						<h4 class="text-center">Ventas</h4>
-						<div class="col-md-4">
+						<div class="col-md-4 text-center">
 								<h6 class="text-center">Pedidos</h6>
+								<a class="text-center" title="Pedidos" href="modulos/mod_venta/pedidosListado.php"><img width="45px" src="css/img/folder-21035081.svg" alt="Pedidos" /></a>
 							</div>
-							<div class="col-md-4">
+							<div class="col-md-4 text-center">
 								<h6 class="text-center">Albaranes</h6>
+								<a class="text-center" title="Albaranes" href="modulos/mod_venta/albaranesListado.php"><img width="45px" src="css/img/folder-236091.svg" alt="Albaranes" /></a>
 							</div>
-							<div class="col-md-4">
+							<div class="col-md-4 text-center">
 								<h6 class="text-center">Facturas</h6>
+								<a class="text-center" title="Factura" href="modulos/mod_venta/facturasListado.php"><img width="35px" src="css/img/document-2034327.svg" alt="Factura" /></a>
 							</div>
 						</div>
 						
@@ -85,7 +91,7 @@
 							</div>
 							<div class="col-md-4 text-center">
 								<h6 class="text-center">Cierres</h6>
-								<a class="text-center" title="Tickets" href="modulos/mod_cierres/CierreCaja.php?dedonde=tpv"><img width="50px" src="css/img/castle-378353.svg" alt="Cierres" /></a>
+								<a class="text-center" title="Cierres" href="modulos/mod_cierres/CierreCaja.php?dedonde=tpv"><img width="50px" src="css/img/castle-378353.svg" alt="Cierres" /></a>
 							</div>
 							<div class="col-md-4 text-center">
 								<h6 class="text-center">Tikets</h6>
@@ -103,7 +109,7 @@
 							</div>
 							<div class="col-md-4 text-center">
 								<h6 class="text-center">Clientes</h6>
-									<a class="text-center" title="Clientes" href="modulos/mod_cliente/ListaClientes.php"><img width="40px" src="css/img/user-1633249.svg" alt="Clientes" /></a>
+									<a class="text-center" title="Clientes" href="modulos/mod_cliente/ListaClientes.php"><img width="40px" src="css/img/handshake-651818.svg" alt="Clientes" /></a>
 							</div>
 							<div class="col-md-4 text-center ">
 								<h6 class="text-center">Proveedores</h6>
@@ -117,13 +123,15 @@
 						<h4 class="text-center">Sistema</h4>
 						<div class="col-md-4 text-center ">
 								<h6 class="text-center">Tiendas</h6>
-									<a class="text-center" title="Tienda" href="modulos/mod_tienda/ListaTiendas.php"><img width="40px" src="css/img/home-146585.svg" alt="Proveedores" /></a>
+									<a class="text-center" title="Tienda" href="modulos/mod_tienda/ListaTiendas.php"><img width="40px" src="css/img/tienda2.svg" alt="Tiendas" /></a>
 							</div>
 							<div class="col-md-4 text-center ">
 								<h6 class="text-center">Incidendias</h6>
+								<a class="text-center" title="Incidencias" href="modulos/mod_incidencias/ListadoIncidencias.php"><img width="45px" src="css/img/attention-3038612.svg" alt="Incidencias" /></a>
 							</div>
 							<div class="col-md-4 text-center ">
-								<h6 class="text-center">Tablas Principales</h6>
+								<h6 class="text-center">Tablas</h6>
+								<a class="text-center" title="Tablas" href="modulos/mod_configuracion/tablasPrincipales.php"><img width="45px" src="css/img/database-schema-1895779.svg" alt="Tablas" /></a>
 							</div>
 						</div>
 						
