54d2b432410254f5ba43384727d302871bf25776
Natalia <nataliaglez92@gmail.com>
2018-06-12 18:02:09 +0200

    Modificación de imagenes todas del mismo tamaño 	new file:   css/img/PedidosVenP.png 	new file:   css/img/albaranComP.png 	new file:   css/img/albaranVenP.png 	modified:   css/img/attention-3038612.svg 	new file:   css/img/cajaP.png 	new file:   css/img/cierresP.png 	new file:   css/img/clientesP.png 	new file:   css/img/facturaVenP.png new file:   css/img/facturasComP.png 	new file:   css/img/familiasP.png 	new file:   css/img/incidenciasP.png 	new file:   css/img/lotesP.png 	new file:   css/img/pedidosComP.png 	new file:   css/img/productosP.png 	new file:   css/img/proveedoresP.png 	copied:     css/img/attention-3038612.svg -> css/img/svg/attention-3038612.svg 	renamed:    css/img/avatar-1299805.svg -> css/img/svg/avatar-1299805.svg 	renamed:    css/img/bar-code-150961.svg -> css/img/svg/bar-code-150961.svg 	renamed:    css/img/basket-1520891.svg -> css/img/svg/basket-1520891.svg 	renamed:    css/img/boy-1300621.svg -> css/img/svg/boy-1300621.svg 	renamed:    css/img/cash-register-576159.svg -> css/img/svg/cash-register-576159.svg 	renamed:    css/img/castle-378353.svg -> css/img/svg/castle-378353.svg 	renamed:    css/img/database-schema-1895779.svg -> css/img/svg/database-schema-1895779.svg 	renamed:    css/img/desk-44076.svg -> css/img/svg/desk-44076.svg 	renamed:    css/img/dibujo1.svg -> css/img/svg/dibujo1.svg 	renamed:    css/img/document-2034327.svg -> css/img/svg/document-2034327.svg 	renamed:    css/img/folder-21035081.svg -> css/img/svg/folder-21035081.svg 	renamed:    css/img/folder-236091.svg -> css/img/svg/folder-236091.svg 	renamed:    css/img/handshake-651818.svg -> css/img/svg/handshake-651818.svg 	renamed:    css/img/home-146585.svg -> css/img/svg/home-146585.svg 	renamed:    css/img/list-1479042.svg -> css/img/svg/list-1479042.svg 	renamed:    css/img/pen-1586721.svg -> css/img/svg/pen-1586721.svg 	renamed:    css/img/ticket-150090.svg -> css/img/svg/ticket-150090.svg 	renamed:    css/img/tienda2.svg -> css/img/svg/tienda2.svg 	renamed:    css/img/van-19185551.ai.svg -> css/img/svg/van-19185551.ai.svg 	new file:   css/img/tablasP.png 	new file:   css/img/ticketsP.png 	new file:   css/img/tiendaP.png 	new file:   css/img/usuariosP.png 	modified:   index.php

--- index.php
+++ index.php
@@ -31,15 +31,15 @@
 						<h4 class="text-center">Almacén</h4>
 							<div class="col-md-4 text-center">
 								<h6 class="text-center">Productos</h6>
-								<a class="text-center" title="Productos" href="modulos/mod_producto/ListaProductos.php"><img width="40px" src="css/img/basket-1520891.svg" alt="Productos" /></a>
+								<a class="text-center" title="Productos" href="modulos/mod_producto/ListaProductos.php"><img  src="css/img/productosP.png" alt="Productos" /></a>
 							</div>
 							<div class="col-md-4 text-center">
 								<h6 class="text-center">Familias</h6>
-								<a class="text-center" title="Familias" href="modulos/mod_familia/ListaFamilias.php"><img width="40px" src="css/img/dibujo1.svg" alt="Familias" /></a>
+								<a class="text-center" title="Familias" href="modulos/mod_familia/ListaFamilias.php"><img  src="css/img/familiasP.png" alt="Familias" /></a>
 							</div>
 							<div class="col-md-4 text-center">
 								<h6 class="text-center">Lotes</h6>
-								<a class="text-center" title="Lotes" href="modulos/mod_etiquetado/ListadoEtiquetas.php"><img width="50px" src="css/img/bar-code-150961.svg" alt="Lotes" /></a>
+								<a class="text-center" title="Lotes" href="modulos/mod_etiquetado/ListadoEtiquetas.php"><img  src="css/img/lotesP.png" alt="Lotes" /></a>
 							</div>
 						</div>
 						
@@ -49,15 +49,15 @@
 						<h4 class="text-center">Compras</h4>
 						<div class="col-md-4 text-center">
 								<h6 class="text-center">Pedidos</h6>
-								<a class="text-center" title="Pedidos" href="modulos/mod_compras/pedidosListado.php"><img width="35px" src="css/img/list-1479042.svg" alt="Pedidos" /></a>
+								<a class="text-center" title="Pedidos" href="modulos/mod_compras/pedidosListado.php"><img  src="css/img/pedidosComP.png" alt="Pedidos" /></a>
 							</div>
 							<div class="col-md-4 text-center">
 								<h6 class="text-center">Albaranes</h6>
-								<a class="text-center" title="Albaran" href="modulos/mod_compras/albaranesListado.php"><img width="45px" src="css/img/desk-44076.svg" alt="Albaran" /></a>
+								<a class="text-center" title="Albaran" href="modulos/mod_compras/albaranesListado.php"><img src="css/img/albaranComP.png" alt="Albaran" /></a>
 							</div>
 							<div class="col-md-4 text-center">
 								<h6 class="text-center">Facturas</h6>
-								<a class="text-center" title="Factura" href="modulos/mod_compras/facturasListado.php"><img width="25px" src="css/img/pen-1586721.svg" alt="Factura" /></a>
+								<a class="text-center" title="Factura" href="modulos/mod_compras/facturasListado.php"><img  src="css/img/facturasComP.png" alt="Factura" /></a>
 							</div>
 						</div>
 						
@@ -67,15 +67,15 @@
 						<h4 class="text-center">Ventas</h4>
 						<div class="col-md-4 text-center">
 								<h6 class="text-center">Pedidos</h6>
-								<a class="text-center" title="Pedidos" href="modulos/mod_venta/pedidosListado.php"><img width="45px" src="css/img/folder-21035081.svg" alt="Pedidos" /></a>
+								<a class="text-center" title="Pedidos" href="modulos/mod_venta/pedidosListado.php"><img  src="css/img/PedidosVenP.png" alt="Pedidos" /></a>
 							</div>
 							<div class="col-md-4 text-center">
 								<h6 class="text-center">Albaranes</h6>
-								<a class="text-center" title="Albaranes" href="modulos/mod_venta/albaranesListado.php"><img width="45px" src="css/img/folder-236091.svg" alt="Albaranes" /></a>
+								<a class="text-center" title="Albaranes" href="modulos/mod_venta/albaranesListado.php"><img  src="css/img/albaranVenP.png" alt="Albaranes" /></a>
 							</div>
 							<div class="col-md-4 text-center">
 								<h6 class="text-center">Facturas</h6>
-								<a class="text-center" title="Factura" href="modulos/mod_venta/facturasListado.php"><img width="35px" src="css/img/document-2034327.svg" alt="Factura" /></a>
+								<a class="text-center" title="Factura" href="modulos/mod_venta/facturasListado.php"><img  src="css/img/facturaVenP.png" alt="Factura" /></a>
 							</div>
 						</div>
 						
@@ -88,15 +88,15 @@
 						<h4 class="text-center text-center">Tickets</h4>
 						<div class="col-md-4 text-center">
 								<h6 class="text-center">Caja</h6>
-								<a class="text-center" title="Caja" href="modulos/mod_tpv/tpv.php"><img width="45px" src="css/img/cash-register-576159.svg" alt="Caja" /></a>
+								<a class="text-center" title="Caja" href="modulos/mod_tpv/tpv.php"><img  src="css/img/cajaP.png" alt="Caja" /></a>
 							</div>
 							<div class="col-md-4 text-center">
 								<h6 class="text-center">Cierres</h6>
-								<a class="text-center" title="Cierres" href="modulos/mod_cierres/CierreCaja.php?dedonde=tpv"><img width="50px" src="css/img/castle-378353.svg" alt="Cierres" /></a>
+								<a class="text-center" title="Cierres" href="modulos/mod_cierres/CierreCaja.php?dedonde=tpv"><img  src="css/img/cierresP.png" alt="Cierres" /></a>
 							</div>
 							<div class="col-md-4 text-center">
 								<h6 class="text-center">Tikets</h6>
-								    <a title="Tickets" href="modulos/mod_tpv/ListaTickets.php?estado=Cobrado"><img width="50px" src="css/img/ticket-150090.svg" alt="Tickets" /></a>
+								    <a title="Tickets" href="modulos/mod_tpv/ListaTickets.php?estado=Cobrado"><img  src="css/img/ticketsP.png" alt="Tickets" /></a>
 							</div>
 						</div>
 						
@@ -106,15 +106,15 @@
 						<h4 class="text-center">Contactos</h4>
 						<div class="col-md-4 text-center">
 								<h6 class="text-center">Usuarios</h6>
-								<a class="text-center" title="Usuarios" href="modulos/mod_usuario/ListaUsuarios.php"><img width="40px" src="css/img/avatar-1299805.svg" alt="Usuarios" /></a>
+								<a class="text-center" title="Usuarios" href="modulos/mod_usuario/ListaUsuarios.php"><img  src="css/img/usuariosP.png" alt="Usuarios" /></a>
 							</div>
 							<div class="col-md-4 text-center">
 								<h6 class="text-center">Clientes</h6>
-									<a class="text-center" title="Clientes" href="modulos/mod_cliente/ListaClientes.php"><img width="40px" src="css/img/handshake-651818.svg" alt="Clientes" /></a>
+									<a class="text-center" title="Clientes" href="modulos/mod_cliente/ListaClientes.php"><img  src="css/img/clientesP.png" alt="Clientes" /></a>
 							</div>
 							<div class="col-md-4 text-center ">
 								<h6 class="text-center">Proveedores</h6>
-								<a class="text-center" title="Proveedores" href="modulos/mod_proveedor/ListaProveedores.php"><img width="60px" src="css/img/van-19185551.ai.svg" alt="Proveedores" /></a>
+								<a class="text-center" title="Proveedores" href="modulos/mod_proveedor/ListaProveedores.php"><img  src="css/img/proveedoresP.png" alt="Proveedores" /></a>
 							</div>
 						</div>
 						
@@ -124,15 +124,15 @@
 						<h4 class="text-center">Sistema</h4>
 						<div class="col-md-4 text-center ">
 								<h6 class="text-center">Tiendas</h6>
-									<a class="text-center" title="Tienda" href="modulos/mod_tienda/ListaTiendas.php"><img width="40px" src="css/img/tienda2.svg" alt="Tiendas" /></a>
+									<a class="text-center" title="Tienda" href="modulos/mod_tienda/ListaTiendas.php"><img src="css/img/tiendaP.png" alt="Tiendas" /></a>
 							</div>
 							<div class="col-md-4 text-center ">
 								<h6 class="text-center">Incidendias</h6>
-								<a class="text-center" title="Incidencias" href="modulos/mod_incidencias/ListadoIncidencias.php"><img width="45px" src="css/img/attention-3038612.svg" alt="Incidencias" /></a>
+								<a class="text-center" title="Incidencias" href="modulos/mod_incidencias/ListadoIncidencias.php"><img src="css/img/incidenciasP.png" alt="Incidencias" /></a>
 							</div>
 							<div class="col-md-4 text-center ">
 								<h6 class="text-center">Tablas</h6>
-								<a class="text-center" title="Tablas" href="modulos/mod_configuracion/tablasPrincipales.php"><img width="45px" src="css/img/database-schema-1895779.svg" alt="Tablas" /></a>
+								<a class="text-center" title="Tablas" href="modulos/mod_configuracion/tablasPrincipales.php"><img  src="css/img/tablasP.png" alt="Tablas" /></a>
 							</div>
 						</div>
 						
