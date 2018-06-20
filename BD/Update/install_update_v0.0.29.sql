17d1701f380a94d51c872877bccc664d5b549846
Ricardo Oficina <ricardolau6@gmail.com>
2018-06-20 23:38:46 +0200

    Elimino ficheros estructura de BD y creo fichero update. Quito punto y coma que sobra.

---
+++ BD/Update/install_update_v0.0.29.sql
@@ -0,0 +1,16 @@
+ALTER TABLE `articulosStocks` ADD `fechaRegularizacion` DATETIME NULL AFTER `fecha_modificado`, ADD `usuarioRegularizacion` INT NULL AFTER `fechaRegularizacion`;
+
+CREATE TABLE `articulosRegularizaciones` (
+  `id` int(11) NOT NULL,
+  `idArticulo` int(11) NOT NULL,
+  `idTienda` int(11) NOT NULL DEFAULT '1',
+  `fechaRegularizacion` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `stockActual` decimal(17,6) NOT NULL,
+  `stockModif` decimal(17,6) NOT NULL,
+  `stockFinal` decimal(17,6) NOT NULL,
+  `stockOperacion` int(1) NOT NULL DEFAULT '1',
+  `idUsuario` int(11) NOT NULL,
+  `creado_en` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `actualizado_en` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
