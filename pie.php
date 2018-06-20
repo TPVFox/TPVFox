f78a29d688a1b577443b2c13a037a2863f875519
Natalia <nataliaglez92@gmail.com>
2018-06-20 16:58:43 +0200

    Añadido diseño pie de página 	modified:   css/template.css 	new file:   pie.php

---
+++ pie.php
@@ -0,0 +1,16 @@
+<footer class=" mb-2 piePagina">
+  <div class="container-fluid text-center text-md-left ">
+    <div class="row">
+      <div class="col-md-4 mt-md-0 mt-3 ">
+        <p>licence: GPL2_1.0</p>
+      </div>
+      <div class="col-md-4 mt-md-0 mt-3">
+        <p>Tpvfox - Gestión de empresas</p>
+      </div>
+      <div class="col-md-4 mt-md-0 mt-3">
+        <a href="<?php echo $HostNombre;?>/estatico"> Créditos</a>
+      </div>
+    </div>
+  </div>
+</footer>
+
