658d21fdf08246339f41f914a5cf5112fcdb6201
Ricardo Oficina <ricardolau6@gmail.com>
2018-06-27 22:55:51 +0200

    Modifico tabla articulos clientes.

---
+++ BD/Update/install_update_v0.0.30.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `facproltemporales` CHANGE `Albaranes` `Albaranes` VARBINARY(50000) NULL DEFAULT NULL;
+ALTER TABLE `articulosClientes` CHANGE `fechaActualizacion` `fechaActualizacion` DATETIME NOT NULL;
