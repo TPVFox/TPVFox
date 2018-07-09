2b3e1f8004895f8185ec79c446158c42606693ef
Ricardo Oficina <ricardolau6@gmail.com>
2018-07-10 00:36:40 +0200

    Añado metodo para obtener el objeto de un plugin.

--- modulos/mod_producto/clases/ClaseProductos.php
+++ modulos/mod_producto/clases/ClaseProductos.php
@@ -135,6 +135,24 @@ class ClaseProductos extends ClaseTablaArticulos{
 		$plugins = $this->plugins;
 		return $plugins;//->GetDir();
 	}
+
+    public function SetPlugin($nombre_plugin){
+        // @ Objetivo
+        // Devolver el Object del plugin en cuestion.
+        // @ nombre_plugin -> (string) Es el nombre del plugin que hay parametros de este.
+        // Devuelve:
+        // Puede devolcer Objeto  o boreano false.
+        $Obj = false;
+        if (count($this->plugins)>0){
+            foreach ($this->plugins as $plugin){
+                if ($plugin['datos_generales']['nombre_fichero_clase'] === $nombre_plugin){
+                    $Obj = $plugin['clase'];
+                }
+            }
+        }
+        return $Obj;
+
+    }
 	
 	public function GetView(){
 		return $this->view;//->GetDir();
