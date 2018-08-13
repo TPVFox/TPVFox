cae0af300abeaec5700195ac96cef3e2852a6a10
Natalia <nataliaglez92@gmail.com>
2018-08-13 17:48:27 +0200

    Ordenar productos por orden alfabetico 	modified:   modulos/mod_cliente/Resumenes/resumenTickets.php

--- modulos/mod_cliente/Resumenes/resumenTickets.php
+++ modulos/mod_cliente/Resumenes/resumenTickets.php
@@ -188,7 +188,12 @@
 						<?php 
 						$totalProductos=0;
 						if(isset($arrayNums['productos'])){
+                              foreach ($arrayNums['productos'] as $key => $row) {
+                                    $aux[$key] = $row['cdetalle'];
+                                }
+                                array_multisort($aux, SORT_ASC, $arrayNums['productos']);
 							foreach($arrayNums['productos'] as $producto){
+                            
 								$precio=$producto['totalUnidades']*$producto['precioCiva'];
 								echo '<tr>'
 								. '<td>'.$producto['cdetalle'].'</td>'
