1873a04691fc375b39c7d33da58f633817e3b389
Natalia <nataliaglez92@gmail.com>
2018-08-31 21:28:04 +0200

    nueva vista de importar 	new file:   modulos/mod_importar_virtuemart/Importar_virtuemart.php

---
+++ modulos/mod_importar_virtuemart/Importar_virtuemart.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<?php
+    include_once './../../inicial.php';
+    include_once $URLCom.'/head.php';
+    include ($URLCom.'/controllers/Controladores.php');
+    include_once ($URLCom.'/modulos/mod_importar_virtuemart/funciones.php');
+    
+    
+	
+?>
+</head>
+<body>
+      <?php
+            include_once $URLCom.'/modulos/mod_menu/menu.php';
+        ?>
+    <div class="container">
+      
+        <h2 class="text-center">Importación o Actualizacion de datos de Virtuemart a TPV.</h2>
+        <div class="col-md-5">
+            <h3>Parametros a configurar</h3>
+            <div class="col-md-12">
+            </div>
+        </div>
+        <div class="col-md-7">
+            <h3>Proceso</h3>
+            <div class="col-md-12">
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
