4ecd808f76638428039c7183658524aee411c7ec
Natalia <nataliaglez92@gmail.com>
2018-09-18 20:38:50 +0200

    sentencia de modificación de tipo productos a los productos de familia 	modified:   BD/Update/install_update_v0.0.40.sql

--- BD/Update/install_update_v0.0.40.sql
+++ BD/Update/install_update_v0.0.40.sql
@@ -6,3 +6,5 @@ CREATE TABLE `tpvfox_provincial`.`modulo_balanza` ( `idBalanza` INT NOT NULL AUT
 CREATE TABLE `tpvfox_provincial`.`modulo_balanza_plus` ( `idBalanza` INT NOT NULL , `plu` INT(10) NOT NULL , `tecla` INT(100) NOT NULL , `idArticulo` INT NOT NULL ) ENGINE = InnoDB;
 
 UPDATE `articulos` SET tipo='unidad'
+
+UPDATE `articulos` as a inner join articulosFamilias as b on a.idArticulo=b.idArticulo set a.tipo='peso' where b.idFamilia in (28, 214, 25, 24, 26, 215, 444, 112, 85, 217, 27, 34, 54, 33, 35, 159, 160, 158, 62, 59, 60, 357, 387, 58, 380, 388, 356, 386, 61, 404, 379, 401, 374, 378, 399, 403, 372, 402, 414, 373)
