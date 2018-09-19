b3d762c934006de44dda0a987de852fafdbef57f
Natalia <nataliaglez92@gmail.com>
2018-09-19 17:59:16 +0200

    Añadida la clase de balanzas con la función de añadir balanza 	new file:   modulos/mod_balanza/clases/ClaseBalanza.php 	modified:   modulos/mod_balanza/tareas.php

---
+++ modulos/mod_balanza/clases/ClaseBalanza.php
@@ -0,0 +1,16 @@
+<?php 
+include_once $RutaServidor.$HostNombre.'/modulos/claseModelo.php';
+
+class ClaseBalanza extends Modelo  {
+    public function addBalanza($datos){
+        $sql='INSERT INTO `modulo_balanza`(`nombreBalanza`, `modelo`, `conTecla`) VALUES ("'.$datos['nombreBalanza'].'", 
+        "'.$datos['modeloBalanza'].'", "'.$datos['teclas'].'")';
+        $consulta = $this->consultaDML($sql);
+        if (isset($consulta['error'])) {
+            return $consulta;
+        }
+    }
+}
+
+
+?>
--- modulos/mod_balanza/tareas.php
+++ modulos/mod_balanza/tareas.php
@@ -1,11 +1,18 @@
 <?php 
 include_once './../../inicial.php';
-
+include_once $URLCom . '/modulos/mod_balanza/clases/ClaseBalanza.php';
+$CBalanza=new ClaseBalanza($BDTpv);
 $pulsado = $_POST['pulsado'];
 $respuesta=array();
 switch ($pulsado) {
     case 'addBalanza':
-        
+        $datos=array(
+            'nombreBalanza'=>$_POST['nombreBalanza'],
+            'modeloBalanza'=>$_POST['modeloBalanza'],
+            'teclas'        =>$_POST['teclas']
+        );
+        $addBalanza=$CBalanza->addBalanza($datos);
+        $respuesta['balanza']=$addBalanza;
     break;
 }
 echo json_encode($respuesta);
