0cddc601a77c6fb63f7da7ba2d72609045f87f50
Ricardo Oficina <ricardolau6@gmail.com>
2018-10-09 22:12:14 +0200

    Corrijo error de update un punto y coma.

--- BD/Update/install_update_v0.0.42a.sql
+++ BD/Update/install_update_v0.0.42a.sql
@@ -1,2 +1,2 @@
-DELETE FROM `familiasTienda` WHERE `idTienda`=1 
+DELETE FROM `familiasTienda` WHERE `idTienda`=1;
 ALTER TABLE `familiasTienda` ADD `id` INT NOT NULL AUTO_INCREMENT FIRST, ADD PRIMARY KEY (`id`);
