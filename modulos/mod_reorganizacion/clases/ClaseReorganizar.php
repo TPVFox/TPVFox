f3d466c74791983af12524941883ce7a43c0256e
Guillermo Cordeiro <gcordeiro@librebit.com>
2019-08-11 10:57:15 +0200

    Se modifica la consulta SQL para no usar JOIN.

--- modulos/mod_reorganizacion/clases/ClaseReorganizar.php
+++ modulos/mod_reorganizacion/clases/ClaseReorganizar.php
@@ -48,12 +48,18 @@ class ClaseReorganizar extends TFModelo  {
     }
 
     public function obtenerIdsWeb($inicio,$cantidad){
-        //~ $sql = 'SELECT T.idArticulo,T.idVirtuemart,ifNull(S.stockOn,0) as stockOn FROM `articulosTiendas` as T LEFT join articulosStocks AS S ON S.idArticulo=T.idArticulo WHERE T.idTienda ='.$this->idTiendaWeb.' LIMIT '.$inicio.','.$cantidad;
 
-        $sql = 'SELECT T.idArticulo,T.idVirtuemart,ifNull(S.stockOn,0) as stockOn,p.pvpSiva as pvpSiva '
-                .'FROM `articulosTiendas` as T LEFT join articulosStocks AS S ON S.idArticulo=T.idArticulo '
-                .'LEFT JOIN articulosPrecios as p ON S.idArticulo=p.idArticulo WHERE T.idTienda ='
-                .$this->idTiendaWeb.' LIMIT '.$inicio.','.$cantidad;
+        $sql = 'SELECT T.idArticulo, T.idVirtuemart, (' .
+                    'SELECT IFNULL( stockOn, 0 ) ' .
+                    'FROM `articulosStocks AS S ' .
+                    'WHERE T.idArticulo = P.idArticulo ' .
+               ') as stocksOn, ( ' .
+                    'SELECT pvpSiva ' .
+                    'FROM `articulosPrecios` AS P ' .
+                    'WHERE T.idArticulo = P.idArticulo ' .
+               'FROM `articulosTienda` AS T ' .
+               'WHERE T.idTienda = ' . $this->idTiendaWeb .
+               ' LIMIT ' . $inicio . ',' . $cantidad;
 
         $resultado = $this->consulta($sql);
         return $resultado;
