39681586c6867b62855d8db81aaa1d8a816e02cd
Ricardo Oficina <ricardolau6@gmail.com>
2019-08-23 02:48:25 +0200

    Error de ;

--- modulos/mod_producto/tareas.php
+++ modulos/mod_producto/tareas.php
@@ -265,7 +265,7 @@ switch ($pulsado) {
                 }
                 
             }else{
-                $respuesta['advertencia']='Revisa estado producto, tiene que esta baja' 
+                $respuesta['advertencia']='Revisa estado producto, tiene que esta baja'; 
                 array_push( $productosNoEliminados, $datos);
             }
            
