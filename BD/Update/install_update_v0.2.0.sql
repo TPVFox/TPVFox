2f9889ea15bcb41b098fa63c2574693f7c565a4e
Ricardo Oficina <ricardolau6@gmail.com>
2019-09-19 01:36:28 +0200

    Cambio en estructura , ya que permito NULL en en ese campo.

--- BD/Update/install_update_v0.2.0.sql
+++ BD/Update/install_update_v0.2.0.sql
@@ -1,2 +1,3 @@
 # Aportación de Guillermo que asi vuela.... :-)
 alter table articulosPrecios add primary key (idArticulo, idTienda);
+ALTER TABLE `articulosTiendas` CHANGE `crefTienda` `crefTienda` VARCHAR(18) CHARACTER SET utf8 COLLATE utf8_general_ci NULL;
