f120937ae214393d050fdaffa83aaa074be1570f
Ricardo Oficina <ricardolau6@gmail.com>
2020-01-28 00:53:08 +0100

    Los update los pongo juntos.

--- BD/Update/install_update_v0.2.22.sql
+++ BD/Update/install_update_v0.2.22.sql
@@ -6,8 +6,10 @@ ALTER TABLE `pedproIva` CHANGE `Numpedpro` `Numpedpro` INT(11) NULL;
 ALTER TABLE `pedprolinea` CHANGE `Numpedpro` `Numpedpro` INT(11) NULL; 
 # Añado campos para controlar quien modifica un pedido, albaran o factura de un proveedor.
 ALTER TABLE `pedprot` ADD `modify_by` INT NULL AFTER `fechaModificacion`; 
-# Si hay albaranes temporales hay que cubvrir el campo fecha.
-UPDATE `albproltemporales` SET `Fecha`=fechaInicio WHERE 1;
 ALTER TABLE `albprolinea` CHANGE `Numpedpro` `idpedpro` INT(10) NULL DEFAULT NULL; 
 ALTER TABLE `albprot` ADD `fechaModificacion` DATETIME NULL AFTER `FechaVencimiento`;
 ALTER TABLE `albprot` ADD `modify_by` INT NULL AFTER `fechaModificacion`; 
+ALTER TABLE `facproltemporales` ADD `Fecha` DATETIME DEFAULT NULL AFTER `idUsuario`;
+# Si hay temporales hay que cubrir el campo fecha.
+UPDATE `albproltemporales` SET `Fecha`=fechaInicio WHERE 1;
+UPDATE `facproltemporales` SET `Fecha`=fechaInicio WHERE 1;
