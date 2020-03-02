8e1cc2d4491760ee12a2fc695bb9e71f7ba89b8e
Ricardo Oficina <ricardolau6@gmail.com>
2020-03-02 13:19:26 +0100

    Añado cambios en BD para que funciones importaciones csv

--- BD/Update/install_update_v0.2.23.sql
+++ BD/Update/install_update_v0.2.23.sql
@@ -1,3 +1,11 @@
 ALTER TABLE `facprot` ADD `total_siniva` DECIMAL(17,6) NOT NULL AFTER `entregado`;
 ALTER TABLE `albprot` ADD `total_siniva` DECIMAL(17,6) NOT NULL AFTER `entregado`;
 ALTER TABLE `pedprot` ADD `total_siniva` DECIMAL(17,6) NOT NULL AFTER `entregado`;
+# Cambios para que funcione importacion csv
+ALTER TABLE `proveedores` CHANGE `movil` `movil` VARCHAR(11) CHARACTER SET utf8 COLLATE utf8_general_ci NULL;
+ALTER TABLE `proveedores` CHANGE `fax` `fax` VARCHAR(11) CHARACTER SET utf8 COLLATE utf8_general_ci NULL;
+ALTER TABLE `proveedores` CHANGE `email` `email` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL;
+ALTER TABLE `proveedores` CHANGE `telefono` `telefono` VARCHAR(11) CHARACTER SET utf8 COLLATE utf8_general_ci NULL;
+ALTER TABLE `articulosCodigoBarras` ADD `id` INT NOT NULL AUTO_INCREMENT FIRST, ADD PRIMARY KEY (`id`);
+ALTER TABLE `articulosPrecios` DROP PRIMARY KEY, ADD INDEX (`idArticulo`, `idTienda`) USING BTREE;
+ALTER TABLE `articulosPrecios` ADD `id` INT NOT NULL AUTO_INCREMENT FIRST, ADD PRIMARY KEY (`id`); 
