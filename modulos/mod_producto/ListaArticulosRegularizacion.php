6f3cf9296628750db30dfb5e96f4b40cd1c2e0e5
Ricardo Oficina <ricardolau6@gmail.com>
2021-02-27 00:48:19 +0100

    Preparo vistas para poder utilizar plantilla

--- modulos/mod_producto/ListaArticulosRegularizacion.php
+++ modulos/mod_producto/ListaArticulosRegularizacion.php
@@ -1,16 +1,14 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <?php
+<?php
         include './../../inicial.php';
-        include './../../head.php';
         include './funciones.php';
         include './clases/ClaseRegularizaciones.php';
-
         $alArticulo = new alArticulosRegularizacion();
         $Productos = $alArticulo->leerTodos(['regu.estado=' . K_STOCKREGULARIZACION_ESTADO_ACTIVO], ['regu.*', 'arts.articulo_name']);
         ?>
-
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php  include './../../head.php';?>
         <script>
             // Declaramos variables globales
             var checkID = [];
@@ -20,14 +18,10 @@
         <script src="<?php echo $HostNombre; ?>/controllers/global.js"></script> 
         <script src="<?php echo $HostNombre; ?>/plugins/modal/func_modal_reutilizables.js"></script>
     </head>
-
     <body>
-        
         <?php
-        //~ include './../../header.php';
         include_once $URLCom.'/modulos/mod_menu/menu.php';
         ?>
-
         <div class="container">
             <div class="row">
                 <div class="col-md-12 text-center">
@@ -40,18 +34,10 @@
                     </div>
 
                 </div>
-
                 <div class="col-md-10">
                     <!-- TABLA DE PRODUCTOS -->
                     <div>
                         <form action="ordenar">
-<!--
-                            <table class="table table-bordered table-hover">
-                                <tr>
-                                    <td><button class="btn btn-sm boton-ordenar" data-idarticulo="<?php //echo $producto['idArticulo']; ?>">Ordenar</button></td>
-                                </tr>
-                            </table>
--->
                         </form>
                         <table class="table table-bordered table-hover">
                             <thead>
@@ -67,15 +53,12 @@
                                     <th>idAlbaran</th>
                                     <th>Estado</th>
                                     <th>botón alguna acción</th>
-
                                 </tr>
                             </thead>
-
                             <?php
                             if (isset($Productos)) {
                                 foreach ($Productos as $producto) {
                                     ?>
-
                                     <tr>
                                         <td></td>
                                         <td><?php echo $producto['id']; ?></td>
@@ -89,51 +72,12 @@
                                         <td><?php echo $producto['estado']; ?></td>
                                         <td><button class="btn btn-sm boton-albaran" data-idarticulo="<?php echo $producto['idArticulo']; ?>">Albarán</button></td>
                                     </tr>
-
                                     <?php
                                 }
                             }
                             ?>
-
                         </table>
                     </div>
-                </div>
-            </div>
-        </div>
-        <!-- Modal -->
-        <div id="regularizaStockModal" class="modal fade" role="dialog">
-            <div class="modal-dialog">
-                <!-- Modal content-->
-                <div class="modal-content">
-                    <div class="modal-header btn-primary">
-                        <button type="button" class="close" data-dismiss="modal">&times;</button>
-                        <h3 class="modal-title text-center">Titulo configurable</h3>
-                    </div>
-                    <form id="fregulariza" action="javascript:grabarRegularizacion();">
-                        <div class="modal-body">
-                            <table>
-                                <tr>
-                                    <td colspan="3">
-                                        <h5>Articulo a regularizar:<p id="nombre" > </p> </h5></td>                                    
-                                </tr>
-                                <tr>
-                                    <td>Stock Actual</td>
-                                    <td>Stock a colocar</td>
-                                    <td>SUMAR al stock</td>
-                                </tr>
-                                <tr>
-                                    <td><input type="text" id="stockactual" value="000" readonly="readonly"/></td>
-                                    <td><input type="text" id="stockcolocar" value="000" /></td>
-                                    <td><input type="text" id="stocksumar" value="000" /></td>
-                                </tr>
-                            </table>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="submit" class="btn btn-default" >Guardar</button>
-                            <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
-                        </div>
-                        <input type="hidden" id="articuloid" value="000" />
-                    </form>
                 </div>
             </div>
         </div>
--- modulos/mod_producto/ListaEtiquetas.php
+++ modulos/mod_producto/ListaEtiquetas.php
@@ -1,28 +1,22 @@
-<!DOCTYPE html>
-<html>
-    <head>
-	<?php
+<?php
     include_once './../../inicial.php';
-    include_once $URLCom.'/head.php';
 	include_once $URLCom.'/modulos/mod_producto/funciones.php';
     include_once $URLCom.'/controllers/Controladores.php';
 	include_once $URLCom.'/modulos/mod_producto/clases/ClaseProductos.php';
     include_once $URLCom.'/clases/articulos.php';
-
-	
 	$CArticulos = new Articulos($BDTpv);
 	$Tienda = $_SESSION['tiendaTpv'];
 	$idTienda= $Tienda['idTienda'];
 	$dedonde="ListaEtiquetas";
-	
-	
-	?>
-	<script src="<?php echo $HostNombre; ?>/modulos/mod_producto/funciones.js"></script>
-    <script src="<?php echo $HostNombre; ?>/controllers/global.js"></script> 
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include_once $URLCom.'/head.php'; ?>
+        <script src="<?php echo $HostNombre; ?>/modulos/mod_producto/funciones.js"></script>
+        <script src="<?php echo $HostNombre; ?>/controllers/global.js"></script> 
     </head>
-
-<body>
-		
+    <body>
         <?php
         include_once $URLCom.'/modulos/mod_menu/menu.php';
         ?>
@@ -34,77 +28,70 @@
 		}
         ?>
        </script>
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12 text-center">
-				<h2> Etiquetas: Imprimir etiquetas del producto </h2>
-			</div>
-			<form action="" method="post" name="formProducto" onkeypress="return anular(event)">
-			<div class="col-sm-2">
-				<a class="text-ritght" href="./ListaProductos.php">Volver Atrás</a>
-				<br><br>
-				Selecciona el tamaño: 
-				<select  name="tamanhos">
-					
-					<option value="1">A5</option>
-                    <?php
-                    if ( $ClasePermisos->getModulo('mod_balanza') == 1) {
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <h2> Etiquetas: Imprimir etiquetas del producto </h2>
+                </div>
+                <form action="" method="post" name="formProducto" onkeypress="return anular(event)">
+                    <div class="col-sm-2">
+                        <a class="text-ritght" href="./ListaProductos.php">Volver Atrás</a>
+                        <br><br>
+                        Selecciona el tamaño: 
+                        <select  name="tamanhos">
+                            
+                            <option value="1">A5</option>
+                            <?php
+                            if ( $ClasePermisos->getModulo('mod_balanza') == 1) {
+                                echo '<option value="1T">A5-Con Tecla</option>';
+                            }
+                            ?>
+                            <option value="2">A7</option>
+                            <?php
+                            if ( $ClasePermisos->getModulo('mod_balanza') == 1) {
+                                echo '<option value="2T">A7-Con Tecla</option>';
+                            }
+                            ?>
+                            <option value="3">A8</option>
+                            <option value="4">A9</option>
+                        </select>
+                        <br><br>
+                        <input type="submit" value="Imprimir" name="Imprimir">
+                    </div>
+                    <div class="col-md-10">
+                    <table class="table table-bordered table-hover">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>PRODUCTO</th>
+                                <th>P.V.P</th>
+                                <th>ELIMINAR</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        <?php
+                            foreach($_SESSION['productos_seleccionados'] as $producto){
+                                $articulo=$CArticulos->buscarNombreArticulo($producto);
+                                $precio=$CArticulos->articulosPrecio($producto);
+                                ?>
+                                <tr>
+                                    <td><?php echo $producto;?></td>
+                                    <td><?php echo $articulo['articulo_name'];?></td>
+                                    <td><?php echo number_format($precio['pvpCiva'],2);?>€</td>
+                                    <td>
+                                        <a onclick="selecionarItemProducto(<?php echo $producto;?>, 'ListaEtiquetas')">
+                                        <span class="glyphicon glyphicon-trash"></span>
+                                        </a>
+                                    </td>
+                                </tr>
+                            <?php
+                            }
                         ?>
-                        <option value="1T">A5-Con Tecla</option>
-                    <?php
-                    }
-                    ?>
-					<option value="2">A7</option>
-                    <?php
-                    if ( $ClasePermisos->getModulo('mod_balanza') == 1) {
-                        ?>
-                        <option value="2T">A7-Con Tecla</option>
-                    <?php
-                    }
-                    ?>
-                    <option value="3">A8</option>
-					<option value="4">A9</option>
-				</select>
-				<br><br>
-				<input type="submit" value="Imprimir" name="Imprimir">
-				
-				
-			</div>
-			<div class="col-md-10">
-			<table class="table table-bordered table-hover">
-				<thead>
-					<tr>
-						<th>ID</th>
-						<th>PRODUCTO</th>
-						<th>P.V.P</th>
-						<th>ELIMINAR</th>
-					</tr>
-				</thead>
-				<tbody>
-				<?php
-					foreach($_SESSION['productos_seleccionados'] as $producto){
-						$articulo=$CArticulos->buscarNombreArticulo($producto);
-						$precio=$CArticulos->articulosPrecio($producto);
-						?>
-						<tr>
-						<td><?php echo $producto;?></td>
-						<td><?php echo $articulo['articulo_name'];?></td>
-						<td><?php echo number_format($precio['pvpCiva'],2);?>€</td>
-						<td>
-							<a onclick="selecionarItemProducto(<?php echo $producto;?>, 'ListaEtiquetas')">
-							<span class="glyphicon glyphicon-trash"></span>
-							</a>
-						</td>
-					</tr>
-					<?php
-					}
-				?>
-				</tbody>
-			</table>
-			</div>
-			</form>
-		</div>
+                        </tbody>
+                    </table>
+                    </div>
+                </form>
+            </div>
 		 </div>
-		
-</body>
+    </body>
 </html>
