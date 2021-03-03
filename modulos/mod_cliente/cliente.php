62cc90353d80138604f34ed56e46d271c7edba80
Ricardo Oficina <ricardolau6@gmail.com>
2021-03-03 22:27:26 +0100

    Arreglo error al crear cliente nuevo.

--- modulos/mod_cliente/cliente.php
+++ modulos/mod_cliente/cliente.php
@@ -1,9 +1,6 @@
-<!DOCTYPE html>
-<html>
-    <head>
+
         <?php
         include_once './../../inicial.php';
-        include_once $URLCom.'/head.php';
         include_once $URLCom.'/modulos/mod_cliente/funciones.php';
         include_once $URLCom.'/controllers/Controladores.php';
         include_once $URLCom.'/controllers/parametros.php';
@@ -21,6 +18,11 @@
 		$configuracion=$configuracion['incidencias'];
         $titulo= "Crear"; 
 		$estados_cliente = array ('Activo','inactivo');
+        if (isset($_GET['id'])) {
+			$id=$_GET['id']; // Obtenemos id para modificar.
+        } else {
+            $_GET['accion'] = 'Nuevo';
+        }
         // [Get_accion] -> editar,ver,Nuevo
         // Si existe accion, variable es $accion , sino es "editar"
         $Get_accion = (isset($_GET['accion']))? $_GET['accion'] : 'ver';
@@ -28,9 +30,7 @@
         if ($Get_accion === 'ver'){
             $solo_lectura = ' readonly';
         }
-        if (isset($_GET['id'])) {
-			$id=$_GET['id']; // Obtenemos id para modificar.
-        }
+        
         if ($id> 0){
             $titulo= "Modificar";
         }
@@ -90,16 +90,24 @@
                 $html_optionEstado .='<option value="'.$estado_cliente.'"'.$es_seleccionado.'>'.$estado_cliente.'</option>';
             }
 		?>
-	</head>
-	<body>
-		<script src="<?php echo $HostNombre; ?>/modulos/mod_cliente/funciones.js"></script>
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php   include_once $URLCom.'/head.php';?>
+        <script src="<?php echo $HostNombre; ?>/modulos/mod_cliente/funciones.js"></script>
 		<script src="<?php echo $HostNombre; ?>/modulos/mod_incidencias/funciones.js"></script>
-		<script type="text/javascript" >
+		<script>
         <?php echo 'var configuracion='.json_encode($configuracion).';';?>	
 		</script>
-		<?php
-         include_once $URLCom.'/modulos/mod_menu/menu.php';
-		?>
+        <?php
+        if ($Get_accion ==='Nuevo'){
+        echo "<script>history.replaceState(null,'','?accion=Nuevo');</script>";
+        }
+        ?>
+		
+	</head>
+	<body>
+		<?php include_once $URLCom.'/modulos/mod_menu/menu.php';?>
 		<div class="container">
             <?php
             if (isset($errores) && count($errores)>0){
@@ -118,137 +126,148 @@
             }
             ?>
 			<h1 class="text-center"> Cliente: <?php echo $titulo;?></h1>
-			<form action="" method="post" name="formCliente">
-					
-			<a class="text-ritght" href="./ListaClientes.php">Volver Atrás</a>
-            <a  class="btn btn-warning" onclick="abrirModalIndicencia('<?php echo $dedonde;?>' , configuracion , 0, <?php echo $id ;?>);">Añadir Incidencia </a>
+            <div class="col-md-12">
+                <div class="col-md-2">
+                    <?php echo $Controler->getHtmlLinkVolver('Volver ');?>
+                </div>
             <?php
-            if ($Get_accion !== 'ver'){ ?>
-                <input type="submit" class="btn btn-primary" value="Guardar" name="Guardar" id="Guardar">
-            <?php
+            if ($Get_accion !=='Nuevo'){
+                echo '<div class="col-md-2">';
+                echo  '<a  class="btn btn-warning" onclick="abrirModalIndicencia('."'".$dedonde."'".' , configuracion , 0,'.$id.');">Añadir Incidencia </a>';
+                echo '</div>';
             }
             ?>
-			
-			<div class="col-md-12">
-				
-				<h4>Datos del cliente con ID: <input size=3 type="text" id="idClientes" name="idClientes" value="<?php echo $ClienteUnico['idClientes'];?>"   readonly></h4>
+            
+            <form method="post" name="formCliente">
+                <div class="col-md-2">
+                <?php
+                if ($Get_accion !== 'ver'){ ?>
+                    <input type="submit" class="btn btn-primary" value="Guardar" name="Guardar" id="Guardar">
+                <?php
+                }
+                ?>
+                </div>
+                <div class="col-md-12">
+                    <h4>Datos del cliente con ID: <input size=3 type="text" id="idClientes" name="idClientes" value="<?php echo $ClienteUnico['idClientes'];?>"   readonly></h4>
 
-				<div class="col-md-1">
-					<?php 
-					// UrlImagen
-					$img = $HostNombre.'/css/img/imgUsuario.png';
-					?>
-					<img src="<?php echo $img;?>" style="width:100%;">
-				</div>
-				<div class="col-md-7">
-					<div class="Datos">
-						<div class="col-md-6 form-group">
-							<label>Nombre Cliente:</label>
-							<input type="text" id="nombre"  name="Nombre" <?php echo $solo_lectura;?> placeholder="nombre" value="<?php echo $ClienteUnico['Nombre'];?>"  required >
-							 <div class="invalid-tooltip-nombre" display="none">
-								No permitimos la doble comilla (") 
-							</div>
-						</div>
-						<div class="col-md-6 form-group">
-							<label>Razon Social:</label> <!--//al enviar con POST los inputs se cogen con name="xx" PRE-->
-							<input  type="text" id="razonsocial" name="razonsocial" <?php echo $solo_lectura;?> placeholder="razon social" value="<?php echo $ClienteUnico['razonsocial'];?>">
-							 <div class="invalid-tooltip-nombre" display="none">
-								No permitimos la doble comilla (") 
-							</div>
-						</div>
-                        <div class="col-md-6 form-group">
-							<label>NIF:</label>
-							<input type="text"	id="nif" name="nif" <?php echo $solo_lectura;?> value="<?php echo $ClienteUnico['nif'];?>">
-						</div>
-                        <div class="col-md-6 form-group">
-							<label>Codigo Postal:</label>
-							<input type="text" id="codigo_postal" name="codpostal" <?php echo $solo_lectura;?> value="<?php echo $ClienteUnico['codpostal'];?>"   >
-						</div>
-						<div class="col-md-6 form-group">
-							<label>Direccion:</label>
-							<textarea id="direccion" name="direccion" <?php echo $solo_lectura;?>><?php echo $ClienteUnico['direccion'];?> </textarea>
-						</div>
-						<div class="col-md-6 form-group">
-							<label>Telefono:</label>
-							<input type="text" id="telefono" name="telefono" <?php echo $solo_lectura;?> value="<?php echo $ClienteUnico['telefono'];?>"   >
-						</div>
-						<div class="col-md-6 form-group">
-							<label>Movil:</label>
-							<input type="text" id="movil" name="movil" <?php echo $solo_lectura;?> value="<?php echo $ClienteUnico['movil'];?>"   >
-						</div>
-						<div class="col-md-6 form-group">
-							<label>Fax:</label>
-							<input type="text" id="fax" name="fax" <?php echo $solo_lectura;?> value="<?php echo $ClienteUnico['fax'];?>"   >
-						</div>
-						<div class="col-md-6 form-group">
-							<label>Email:</label>
-							<input type="text" id="email" name="email" <?php echo $solo_lectura;?> value="<?php echo $ClienteUnico['email'];?>"  >
-						</div>
-						
-						<div class="col-md-6 form-group">
-							<label for="sel1">Forma de pago por defecto: </label>
-							<select class="form-control" name="formapago" id="sel1" <?php echo $solo_lectura;?> style="width: 15em;">
-								<?php 
-								echo $html_optionPago;
-								?>
-								
-							</select>
-							
-						</div>
-						<div class="col-md-6 form-group">
-							<label for="sel1">Estado:</label>
-							<select class="form-control" name="estado" <?php echo $solo_lectura;?> id="sel1" style="width: 14em;">
-								<?php 
-								echo $html_optionEstado;
-								?>
-								
-							</select>
-						</div>
-						<div class="col-md-6 form-group">
-							<label for="sel1">Vencimiento por defecto:</label>
-							<select class="form-control" name="vencimiento" <?php echo $solo_lectura;?> id="sel1" style="width: 15em;">
-								<?php
-                                echo $html_optionVenci;
-								?>
-							</select>
-						</div>
-					</div>
-					
-				</div>
-				<div class="col-md-4">
-					 <div class="panel-group">
-						 <?php 
-						$num = 1 ; // Numero collapse;
-						$titulo = 'Tickets';
-						echo htmlPanelDesplegable($num,$titulo,$tablaHtml[0]);
-						?>
-						<?php 
-						$num = 2 ; // Numero collapse;
-						$titulo = 'Facturas';
-						echo htmlPanelDesplegable($num,$titulo,$tablaHtml[1]);
-						?>
-						<?php 
-						$num = 3 ; // Numero collapse;
-						$titulo = 'Albaranes';
-						echo htmlPanelDesplegable($num,$titulo,$tablaHtml[2]);
-						?>
-						<?php 
-						$num = 4 ; // Numero collapse;
-						$titulo = 'Pedidos';
-						echo htmlPanelDesplegable($num,$titulo,$tablaHtml[3]);
-						?>
-						 </div>
-					<!-- Aquí irá el código de los grupos-->
-					</div>
-				</form>
-			</div>
-			
-		</div>
+                    <div class="col-md-1">
+                        <?php 
+                        // UrlImagen
+                        $img = $HostNombre.'/css/img/imgUsuario.png';
+                        ?>
+                        <img src="<?php echo $img;?>" style="width:100%;">
+                    </div>
+                    <div class="col-md-7">
+                        <div class="Datos">
+                            <div class="col-md-6 form-group">
+                                <label>Nombre Cliente:</label>
+                                <input type="text" id="nombre"  name="Nombre" <?php echo $solo_lectura;?> placeholder="nombre" value="<?php echo $ClienteUnico['Nombre'];?>"  required >
+                                 <div class="invalid-tooltip-nombre" >
+                                    No permitimos la doble comilla (") 
+                                </div>
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <label>Razon Social:</label> <!--//al enviar con POST los inputs se cogen con name="xx" PRE-->
+                                <input  type="text" id="razonsocial" name="razonsocial" <?php echo $solo_lectura;?> placeholder="razon social" value="<?php echo $ClienteUnico['razonsocial'];?>">
+                                 <div class="invalid-tooltip-nombre">
+                                    No permitimos la doble comilla (") 
+                                </div>
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <label>NIF:</label>
+                                <input type="text"	id="nif" name="nif" <?php echo $solo_lectura;?> value="<?php echo $ClienteUnico['nif'];?>">
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <label>Codigo Postal:</label>
+                                <input type="text" id="codigo_postal" name="codpostal" <?php echo $solo_lectura;?> value="<?php echo $ClienteUnico['codpostal'];?>"   >
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <label>Direccion:</label>
+                                <textarea id="direccion" name="direccion" <?php echo $solo_lectura;?>><?php echo $ClienteUnico['direccion'];?> </textarea>
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <label>Telefono:</label>
+                                <input type="text" id="telefono" name="telefono" <?php echo $solo_lectura;?> value="<?php echo $ClienteUnico['telefono'];?>"   >
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <label>Movil:</label>
+                                <input type="text" id="movil" name="movil" <?php echo $solo_lectura;?> value="<?php echo $ClienteUnico['movil'];?>"   >
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <label>Fax:</label>
+                                <input type="text" id="fax" name="fax" <?php echo $solo_lectura;?> value="<?php echo $ClienteUnico['fax'];?>"   >
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <label>Email:</label>
+                                <input type="text" id="email" name="email" <?php echo $solo_lectura;?> value="<?php echo $ClienteUnico['email'];?>"  >
+                            </div>
+                            
+                            <div class="col-md-6 form-group">
+                                <label for="sel1">Forma de pago por defecto: </label>
+                                <select class="form-control" name="formapago" id="sel1" <?php echo $solo_lectura;?> style="width: 15em;">
+                                    <?php 
+                                    echo $html_optionPago;
+                                    ?>
+                                    
+                                </select>
+                                
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <label for="sel1">Estado:</label>
+                                <select class="form-control" name="estado" <?php echo $solo_lectura;?> id="sel2" style="width: 14em;">
+                                    <?php 
+                                    echo $html_optionEstado;
+                                    ?>
+                                    
+                                </select>
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <label for="sel1">Vencimiento por defecto:</label>
+                                <select class="form-control" name="vencimiento" <?php echo $solo_lectura;?> id="sel3" style="width: 15em;">
+                                    <?php
+                                    echo $html_optionVenci;
+                                    ?>
+                                </select>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div class="col-md-4">
+                         <div class="panel-group">
+                             <?php 
+                            $num = 1 ; // Numero collapse;
+                            $titulo = 'Tickets';
+                            echo htmlPanelDesplegable($num,$titulo,$tablaHtml[0]);
+                            ?>
+                            <?php 
+                            $num = 2 ; // Numero collapse;
+                            $titulo = 'Facturas';
+                            echo htmlPanelDesplegable($num,$titulo,$tablaHtml[1]);
+                            ?>
+                            <?php 
+                            $num = 3 ; // Numero collapse;
+                            $titulo = 'Albaranes';
+                            echo htmlPanelDesplegable($num,$titulo,$tablaHtml[2]);
+                            ?>
+                            <?php 
+                            $num = 4 ; // Numero collapse;
+                            $titulo = 'Pedidos';
+                            echo htmlPanelDesplegable($num,$titulo,$tablaHtml[3]);
+                            ?>
+                        </div>
+                        <!-- Aquí irá el código de los grupos-->
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
 		<?php 
 echo '<script src="'.$HostNombre.'/plugins/modal/func_modal.js"></script>';
 include $RutaServidor.'/'.$HostNombre.'/plugins/modal/ventanaModal.php';
-
+?>
+<?php
 include_once $URLCom.'/pie.php';
 ?>
-	</body>
+</body>
+
 </html>
