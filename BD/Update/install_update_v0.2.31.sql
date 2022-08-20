f265730883404819d63f05304ba2d9739b884a1e
Ricardo Oficina <ricardolau6@gmail.com>
2022-08-20 20:38:42 +0200

    Ahora esta el campo ref_prov con la misma cantidad de caracteres.

--- BD/Update/install_update_v0.2.31.sql
+++ BD/Update/install_update_v0.2.31.sql
@@ -1,1 +1,3 @@
 ALTER TABLE `albprolinea` CHANGE `ref_prov` `ref_prov` VARCHAR(24) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL; 
+ALTER TABLE `pedprolinea` CHANGE `ref_prov` `ref_prov` VARCHAR(24) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+ALTER TABLE `facprolinea` CHANGE `ref_prov` `ref_prov` VARCHAR(24) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL; 
