a9e3a574c9ad06023d2891909fdc7ca76e45bed4
Ricardo Oficina <ricardolau6@gmail.com>
2022-11-15 15:10:43 +0100

    Solucionamos error de que no enviarEmail. Utilizamos el mismo codigo imprimirDocumento ya que tenías dos sistemas distintos.

--- modulos/mod_compras/tareas/enviarEmail.php
+++ modulos/mod_compras/tareas/enviarEmail.php
@@ -1,10 +1,6 @@
 <?php
 //@Objetivo:
-        //Imprimir un documento , dependiendo de donde venga se pone el nombre y envía todos los datos  
-        //a la función montarHTMLimprimir que lo que realiza es simplemente montar el html una parte copn la cabecera y 
-        //otra con el cuerpo del documento
-        //debajo cargamos las clases de imprimir y la plantilla una vez generada y lista la plantilla devolvemos la ruta
-        //para así desde javascript poder abrirla
+//Imprimir un documento , dependiendo de donde venga se pone el nombre y envía todos los datos  
         
         $id=$_POST['id'];
         $dedonde=$_POST['dedonde'];
@@ -14,23 +10,14 @@
         $mensaje=$_POST['message'];
         $asunto=$_POST['asunto'];
         $nombreTmp=$dedonde."compras.pdf";
-        $htmlImprimir=montarHTMLimprimir($id, $BDTpv, $dedonde, $idTienda);
-        $cabecera=$htmlImprimir['cabecera'];
+        include_once $URLCom.'/modulos/mod_compras/tareas/imprimirDocumento.php';    
         $margen_top_caja_texto= 56;
-        $html=$htmlImprimir['html'];
         include_once $URLCom.'/controllers/planImprimir.php';
-        
+              
         $fichero = $RutaServidor.$rutatmp.'/'.$nombreTmp;
         
         include_once $URLCom.'/clases/CorreoElectronico.php';
-        //~ $configuracion = CorreoElectronico::leerConfiguracion();
-        //~ echo '<pre>';
-        //~ print_r($configuracion);
-        //~ echo '</pre>';
-        //~ echo '<pre>';
-        //~ print_r($_POST);
-        //~ echo '</pre>';
-      
+             
         $respuesta = CorreoElectronico::enviar($destinatario,$mensaje,$asunto,$fichero);
         //~ echo '<pre>';
         //~ print_r($respuesta);
