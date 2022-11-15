a0b21265dd4f75eb6cfb815d174cf69a0908b8ef
Ricardo Oficina <ricardolau6@gmail.com>
2022-11-15 02:25:02 +0100

    Issue #133 -> Solucionado.

--- modulos/mod_proveedor/Tareas/imprimirResumenAlbaran.php
+++ modulos/mod_proveedor/Tareas/imprimirResumenAlbaran.php
@@ -1,6 +1,8 @@
 <?php 
 //@Objetivo: Generar el documento de impresión de un resumen de tickets
 include_once ($URLCom."/modulos/mod_proveedor/clases/ClaseProveedor.php");
+include_once $URLCom.'/modulos/mod_producto/clases/ClaseProductos.php';
+$CTArticulos = new ClaseProductos($BDTpv);
 $CProveedor= new ClaseProveedor($BDTpv);
 $Tienda = $_SESSION['tiendaTpv'];
 $resultado['tienda']=$Tienda;
@@ -9,6 +11,11 @@ $fechaInicial=$_POST['fechaInicial'];
 $fechaFinal=$_POST['fechaFinal'];
 $errores=array();
 $textoFechas="";
+$style = array( 'C'=>'style="text-align:center;"',
+				'R'=>'style="text-align:right"',
+				'L'=>'style="text-align:left;"'
+);
+
 if(isset($idProveedor)){
 	$datosProveedor=$CProveedor->getProveedor($idProveedor);
 	if(isset($datosProveedor['error'])){
@@ -31,6 +38,7 @@ if(isset($_POST['fechaInicial']) & isset($_POST['fechaFinal'])){
 	}
 	$resultado['post']=$fechaInicial;
 	$arrayNums=$CProveedor->albaranesProveedoresFechas($idProveedor, $fechaInicial, $fechaFinal);
+	$productos = $CProveedor->SumaLineasAlbaranesProveedores($arrayNums['productos'],'KO');
 	if(isset($arrayNums['error'])){
 		$resultado['error']=array ( 'tipo'=>'DANGER!','dato' => $arrayNums['consulta'],
 		 'class'=>'alert alert-danger','mensaje' => 'Error de sql');
@@ -44,33 +52,57 @@ if(isset($_POST['fechaInicial']) & isset($_POST['fechaFinal'])){
 <font size="15">Resumen de albaranes $textoFechas</font><hr>
 <font size="20">$datosProveedor[nombrecomercial]</font><br>
 <table><tr><td><font size="12">$datosProveedor[razonsocial]</font></td>
-<td><font>Dirección de entrega :</font></td></tr>
+<td>Dirección de entrega :</td></tr>
 <tr><td><font size="9"><b>NIF: </b>$datosProveedor[nif]</font></td>
 <td><font size="9">$datosProveedor[direccion]</font></td></tr>
 <tr><td><font size="9"><b>Teléfono: </b>$datosProveedor[telefono]</font></td>
 <td><font size="9">Código Postal: </font></td></tr>
 <tr><td><font size="9">email: $datosProveedor[email]</font></td><td></td></tr></table>
 EOD;
+$html = '<table WIDTH="80%" border="1px">
+<tr>
+<th>ID</th>
+<th width="3%">P</th>
+<th width="50%">PRODUCTO</th>
+<th $style[C]>NºC</th>
+<th>Uds</th>
+<th>Coste</th>
+<th width="6%">CM</th>
+<th width="20%">IMPORTE</th>
+</tr>
 
-		$html=<<<EOD
-<table WIDTH="80%" border="1px"><tr><td WIDTH="60%">Descripción del producto</td><td>Cantidad</td>
-<td>Precio</td><td>Importe</td></tr></table><table  WIDTH="80%" border="1px">
-EOD;
-
-			foreach($arrayNums['productos'] as $producto){
-				$precio=number_format ($producto['totalUnidades']*$producto['costeSiva'],2);
-                $unidades=number_format ($producto['totalUnidades'],2);
-                $costeiva=number_format ($producto['costeSiva'],2);
-				$html.=<<<EOD
-<tr><td WIDTH="60%"><font size="8">$producto[cdetalle] </font></td>
-<td style="text-align:center;"><font size="8"> $unidades </font></td>
-<td style="text-align:center;"><font size="8">$costeiva</font></td>
-<td style="text-align:center;"><font size="8">$precio</font></td></tr>
-EOD;
-			}
+';
+foreach($productos as $producto){
+	$p =$CTArticulos->GetProducto($producto['idArticulo']);
+	$cdetalle = $p['articulo_name'];
+	//$producto['tipo'] = $p['tipo'];
+	$precio=number_format ($producto['totalUnidades']*$producto['costeSiva'],2);
+	$unidades=number_format ($producto['totalUnidades'],2);
+	$costeiva=number_format ($producto['costeSiva'],2);
+	$html.='
+		<tr>
+		<td><font size="8">'.$producto[idArticulo].'</font></td>
+		<td>';
+		if ($p['proveedor_principal']['idProveedor'] == $idProveedor ){
+		  $html.='*';
+		}
+		$tcF8 = '<td '.$style['C'].'><font size="8">';
+		$html.= '</td>
+		<td WIDTH="50%"><font size="8">'. $cdetalle.'</font></td>'
+		.$tcF8. $producto['num_compras'].'</font></td>'
+		.$tcF8. $unidades.' </font></td>'
+		.$tcF8. $costeiva.'</font></td>
+		<td>';
+		if ($producto['coste_medio'] == 'OK'){
+			$html.='*';
+		}
+		$html.='</td>'
+				.$tcF8.$precio.'</font></td></tr>
+		';
+}
 		$html.=<<<EOD
-</table><table><tr><th></th><th style="text-align:right;">Base</th><th style="text-align:right;">IVA</th>
-<th style="text-align:right;">Total</th></tr>'
+</table><table><tr><th></th><th $style[R]>Base</th><th $style[R]>IVA</th>
+<th $style[R]>Total</th></tr>'
 EOD;
 		$totalLinea=0;
 		$totalDesglose=0;
@@ -78,15 +110,15 @@ EOD;
 				$totalLinea=$desglose['sumBase']+$desglose['sumiva'];
 				$totalDesglose=$totalDesglose+$totalLinea;
 				$html.=<<<EOD
-<tr><td style="text-align:right;">$desglose[iva]%</td><td style="text-align:right;">$desglose[sumBase]</td>
-<td style="text-align:right;">$desglose[sumiva]</td><td style="text-align:right;">$totalLinea</td></tr>
+<tr><td $style[R]>$desglose[iva]%</td><td $style[R]>$desglose[sumBase]</td>
+<td $style[R]>$desglose[sumiva]</td><td $style[R]>$totalLinea</td></tr>
 EOD;
 			}
 		$html .=<<<EOD
-<tr><td></td><td></td><td>TOTAL:</td><td><b>$totalDesglose</b></td></tr></table><h3>Listado de Albaranes</h3>
-<table  WIDTH="75%" border="1px"><tr><td  WIDTH="50%">Fecha</td><td>N_albaran</td><td>Base</td><td>Iva</td>
-<td>Total</td></tr>
-EOD;
+		<tr><td></td><td></td><td>TOTAL:</td><td><b>$totalDesglose</b></td></tr></table><h3>Listado de Albaranes</h3>
+		<table  WIDTH="75%" border="1px"><tr><td  WIDTH="50%">Fecha</td><td>N_albaran</td><td>Base</td><td>Iva</td>
+		<td>Total</td></tr>
+		EOD;
 		$totalLinea=0;
 		$totalbases=0;
 			foreach($arrayNums['resumenBases'] as $bases){
@@ -94,10 +126,10 @@ EOD;
 				$totalbases=$totalbases+$totalLinea;
 				
 				$html.=<<<EOD
-<tr><td WIDTH="50%"><font size="8">$bases[fecha]</font></td><td><font size="8">$bases[Numalbpro]</font></td>
-<td><font size="8">$bases[sumabase]</font></td><td><font size="8">$bases[sumarIva]</font></td>
-<td><font size="8">$totalLinea</font></td></tr>
-EOD;
+					<tr><td WIDTH="50%"><font size="8">$bases[fecha]</font></td><td><font size="8">$bases[Numalbpro]</font></td>
+					<td><font size="8">$bases[sumabase]</font></td><td><font size="8">$bases[sumarIva]</font></td>
+					<td><font size="8">$totalLinea</font></td></tr>
+					EOD;
 			}
 		$html.='</table>';
 		$nombreTmp="Resumen.pdf";
