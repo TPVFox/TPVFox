07ba6b13f93c48a7a5790795911f613d869bd039
Informatica Alagoro <informatica@alagoro.com>
2024-06-03 22:59:05 +0200

    DEV: inicial CRUD tareas. C (de create) R (read) crea y lista

---
+++ BD/Update/install_update_v0.4.0.0.sql
@@ -0,0 +1,11 @@
+--  Alagoro Mayo 2024
+-- Crear tabla tareas Cron
+DROP TABLE IF EXISTS tareas_cron;
+CREATE TABLE tareas_cron (
+ id INT(11) NOT NULL AUTO_INCREMENT ,
+ `nombre` VARCHAR(50) NOT NULL ,
+ `periodo` INT(11) NOT NULL , 
+ `ruta` VARCHAR(50) NOT NULL ,
+ `ultima_ejecucion` DATE NULL, 
+ PRIMARY KEY (`id`)
+ ) ENGINE = InnoDB;
