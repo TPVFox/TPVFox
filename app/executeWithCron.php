34d0e2cfc850cffc247bae6140cc688b65344919
Informatica Alagoro <informatica@alagoro.com>
2024-07-24 22:14:22 +0200

    dev: EN DESARROLLO

--- app/executeWithCron.php
+++ app/executeWithCron.php
@@ -1,10 +1,10 @@
 <?php 
 
-include_once 'inicial.php';
+include_once __DIR__.'/../inicial.php';
 
 include_once $URLCom . '/modulos/mod_tareas_cron/clases/TareasCron.php';
 
 error_log('Paso por aqui--->'.time());        
 
-$tareasCron = new TareasCron();
+$tareasCron = new TareasCron($URLCom);
 $tareasCron->execute();
