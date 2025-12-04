783bd98de86a2f6905b6e0bd0df258a3a2024e3d
acorbaq <anbaco185@gmail.com>
2025-12-04 18:59:49 +0100

    Agregar módulo de temperaturas con configuración y parámetros para gestión de registros y alertas.

---
+++ modulos/mod_temperaturas/temperatura.php
@@ -0,0 +1,25 @@
+<?php
+include_once("./../../inicial.php");
+include_once("./../../configuracion.php");
+//include_once $URLCom . '/modulos/mod_balanza/clases/ClaseGestorDispositivos.php';
+include_once($URLCom . '/controllers/parametros.php');
+include_once $URLCom . '/controllers/Controladores.php';
+
+$ClasesParametros = new ClaseParametros('parametros.xml');
+$Controler = new ControladorComun;
+$Controler->loadDbtpv($BDTpv);
+
+$parametros = $ClasesParametros->getRoot();
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?php include_once $URLCom . '/head.php'; ?>
+</head>
+
+<body>
+    <?php
+    include_once $URLCom . '/modulos/mod_menu/menu.php';
+    ?>
